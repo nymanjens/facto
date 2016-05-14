4ba898a54abcfea208086fdd8921506ffbfb897b
Jens Nyman <nymanjens.nj@gmail.com>
2016-05-14 13:00:26 +0100

    Abandoned another attempt to get ApplicationFunctionalTest to work

--- test/controllers/ApplicationFunctionalTest.scala
+++ test/controllers/ApplicationFunctionalTest.scala
@@ -17,6 +17,23 @@ import models.accounting.config.{MoneyReservoir, Account}
 // TODO: Get this to work.
 class ApplicationFunctionalTest extends PlaySpecification {
 
+  //  "Application" should {
+  //
+  //    "send 404 on a bad request" in new WithApplication {
+  //      val result = route(FakeRequest(GET, "/boum")).get
+  //      status(result) mustEqual NOT_FOUND
+  //    }
+  //
+  //    "render the login page" in new WithApplication {
+  //      val home = route(FakeRequest(GET, "/login/")).get
+  //
+  //      status(home) mustEqual OK
+  //      contentType(home) must beSome.which(_ == "text/html")
+  //      contentAsString(home) must contain ("Login")
+  //      contentAsString(home) must contain ("Please Sign In")
+  //    }
+  //  }
+
   //  "index" in new WithApplication {
   //    val action: EssentialAction = Auth.login
   //
@@ -29,12 +46,12 @@ class ApplicationFunctionalTest extends PlaySpecification {
   //    contentAsString(result) mustEqual "value"
   //  }
 
-//  "index try 2" in new WithApplication {
-//    val fakeRequest = FakeRequest(Helpers.POST, controllers.routes.Auth.login().url, FakeHeaders(), """ {"name": "New Group", "collabs": ["foo", "asdf"]} """)
-//
-//    val result = route(fakeRequest.withCookies(Cookie("myMemberId", "d"))).get
-//
-//    status(result) mustEqual OK
-//    println(contentAsString(result))
-//  }
+  //  "index try 2" in new WithApplication {
+  //    val fakeRequest = FakeRequest(Helpers.POST, controllers.routes.Auth.login().url, FakeHeaders(), """ {"name": "New Group", "collabs": ["foo", "asdf"]} """)
+  //
+  //    val result = route(fakeRequest.withCookies(Cookie("myMemberId", "d"))).get
+  //
+  //    status(result) mustEqual OK
+  //    println(contentAsString(result))
+  //  }
 }
