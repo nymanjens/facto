2f4a5648167852b123300b61bcd8d0628da9a7ac
Jens Nyman <nymanjens.nj@gmail.com>
2016-06-02 17:55:33 +0100

    Added Forwarding implementations EntityManager and QueryableEntityManager

---
+++ app/models/manager/ForwardingEntityManager.scala
@@ -0,0 +1,22 @@
+package models.manager
+
+import scala.collection.immutable.Seq
+
+/** EntityManager base implementation that forwards all calls to a given delegate. */
+abstract class ForwardingEntityManager[E <: Identifiable[E]](delegate: EntityManager[E]) extends EntityManager[E] {
+
+  // ********** Management methods ********** //
+  override def initialize(): Unit = delegate.initialize()
+  override def verifyConsistency(): Unit = delegate.verifyConsistency()
+  override def createSchema: Unit = delegate.createSchema
+  override def tableName: String = delegate.tableName
+
+  // ********** Mutators ********** //
+  override def add(entity: E): E = delegate.add(entity)
+  override def update(entity: E): E = delegate.update(entity)
+  override def delete(entity: E): Unit = delegate.delete(entity)
+
+  // ********** Getters ********** //
+  override def findById(id: Long): E = delegate.findById(id)
+  override def fetchAll(selection: Stream[E] => Stream[E]): Seq[E] = delegate.fetchAll(selection)
+}
