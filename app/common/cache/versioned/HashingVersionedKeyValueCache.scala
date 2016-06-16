a2db8b78fb208ca5c494ce3cc61722f2669f14bf
Jens Nyman <nymanjens.nj@gmail.com>
2016-06-16 17:25:54 +0100

    Documented VersionedKeyValueCache

--- app/common/cache/versioned/HashingVersionedKeyValueCache.scala
+++ app/common/cache/versioned/HashingVersionedKeyValueCache.scala
@@ -3,6 +3,10 @@ package common.cache.versioned
 import com.google.common.hash.HashCode
 import common.cache.UniquelyHashable
 
+/**
+  * VersionedKeyValueCache decorator that stores the unique hashes of the keys and versions, rather than the instances
+  * themselves. This can be useful when the keys or versions are expensive to compare or to keep in memory.
+  */
 private[versioned] final class HashingVersionedKeyValueCache[Key <: UniquelyHashable, Version <: UniquelyHashable,
 Value](delegate: VersionedKeyValueCache[HashCode, HashCode, Value])
   extends VersionedKeyValueCache[Key, Version, Value] {
