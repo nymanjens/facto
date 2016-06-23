bb75873e2414fa47f40517078ba0f867954b16b2
Jens Nyman <nymanjens.nj@gmail.com>
2016-06-23 21:08:22 +0100

    Showing tags in views

--- app/models/accounting/Tag.scala
+++ app/models/accounting/Tag.scala
@@ -1,11 +1,24 @@
 package models.accounting
 
+import com.google.common.hash.Hashing
+
+import math.abs
+import scala.collection.immutable.Seq
 import scala.util.matching.Regex
 
-case class Tag(name: String)
+case class Tag(name: String) {
+  /** Returns the Bootstrap label class for this Tag. **/
+  def labelClass: String = {
+    val hash = Hashing.sha1().hashUnencodedChars(name).asInt()
+    val index = abs(hash) % Tag.labelClassOptions.size
+    Tag.labelClassOptions(index)
+  }
+}
 
 object Tag {
   private val validTagRegex: Regex = """[a-zA-Z0-9-_@$&()+=!.<>;:]+""".r
+  private val labelClassOptions: Seq[String] =
+    Seq("label-primary", "label-success", "label-info", "label-warning", "label-danger")
 
   def isValidTagName(name: String): Boolean = name match {
     case validTagRegex() => true
