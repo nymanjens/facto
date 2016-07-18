52eb6277ee67b8a7dc0c433ec6ff1c74256328c9
Jens Nyman <nymanjens.nj@gmail.com>
2016-07-18 09:28:56 +0100

    Added ReferenceMoney

---
+++ app/models/accounting/money/ReferenceMoney.scala
@@ -0,0 +1,18 @@
+package models.accounting.money
+
+import models.accounting.money.CentOperations.CentOperationsNumeric
+
+case class ReferenceMoney(override val cents: Long) extends CentOperations[ReferenceMoney] {
+
+  override protected def withCents(newCents: Long): ReferenceMoney = ReferenceMoney(newCents)
+
+  override protected def validateCentOperation(that: ReferenceMoney): Unit = {}
+
+  def formatFloat: String = Money.centsToFloatString(cents)
+}
+
+object ReferenceMoney {
+  implicit object MoneyNumeric extends CentOperationsNumeric[ReferenceMoney] {
+    override def fromInt(x: Int): ReferenceMoney = ReferenceMoney(0)
+  }
+}
