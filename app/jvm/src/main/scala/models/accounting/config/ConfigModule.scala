ccbbae3cc5018ece50069ab6e03c241740cd3789
Jens Nyman <nymanjens.nj@gmail.com>
2016-11-07 07:15:18 +0000

    Partial migrated to injected accounting.Config

---
+++ app/jvm/src/main/scala/models/accounting/config/ConfigModule.scala
@@ -0,0 +1,55 @@
+package models.accounting.config
+
+import com.google.inject.{AbstractModule, Provides}
+import collection.immutable.Seq
+import java.nio.file.{Files, Paths}
+
+import play.Play.application
+import play.api.Logger
+import common.ResourceFiles
+
+import com.google.common.base.Throwables
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor
+import org.yaml.snakeyaml.introspector.BeanAccess
+
+import common.Require.requireNonNullFields
+import models.User
+import models.accounting.config.MoneyReservoir.NullMoneyReservoir
+
+final class ConfigModule extends AbstractModule {
+
+  override def configure() = {}
+
+  @Provides()
+  private[config] def config(): Config = {
+    try {
+      // get configLocation
+      val configLocation = application.configuration.getString("facto.accounting.configYamlFilePath")
+
+      // get data
+      val stringData = {
+        if (Files.exists(Paths.get(configLocation))) {
+          scala.io.Source.fromFile(configLocation).mkString
+        } else {
+          require(ResourceFiles.exists(configLocation), s"Could not find $configLocation as file or as resource")
+          ResourceFiles.read(configLocation)
+        }
+      }
+
+      // parse data
+      val constr = new CustomClassLoaderConstructor(getClass.getClassLoader)
+      val yaml = new Yaml(constr)
+      yaml.setBeanAccess(BeanAccess.FIELD)
+      val configData = yaml.load(stringData).asInstanceOf[Parsable.Config]
+
+      // convert to parsed config
+      configData.parse
+    } catch {
+      case e: Throwable =>
+        val stackTrace = Throwables.getStackTraceAsString(e)
+        Logger.error(s"Error when parsing accounting-config.yml: $stackTrace")
+        throw e
+    }
+  }
+}
