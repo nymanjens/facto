864db4505a4fe808e010ecf01738a5f81b82d561
Jens Nyman <nymanjens.nj@gmail.com>
2016-11-14 10:56:22 +0000

    Added EntityManager

---
+++ app/jvm/src/main/scala/models/manager/EntityManager.scala
@@ -0,0 +1,25 @@
+package models.manager
+
+import scala.collection.immutable.Seq
+
+import slick.lifted.{AbstractTable, TableQuery}
+
+import models.SlickUtils.dbApi._
+
+/** Provides access to persisted entries. */
+trait EntityManager[E <: Entity[E]] {
+
+  // ********** Mutators ********** //
+  /** Persists a new entity (without ID) and returns the same entity with its ID in the database. */
+  def add(entity: E): E
+  /** Persists an update to an existing entity and returns the given entity. */
+  def update(entity: E): E
+  /** Deletes an existing entity from the database. */
+  def delete(entity: E): Unit
+
+  // ********** Getters ********** //
+  /** Returns the entity with given ID or throws an exception. */
+  def findById(id: Long): E
+  /** Returns all stored entities. */
+  def fetchAll(): List[E]
+}
--- app/jvm/src/main/scala/models/manager/SlickEntityManager.scala
+++ app/jvm/src/main/scala/models/manager/SlickEntityManager.scala
@@ -7,7 +7,7 @@ import slick.lifted.{AbstractTable, TableQuery}
 import models.SlickUtils.dbApi._
 
 /** Provides access to persisted entries using the Slick API. */
-trait SlickEntityManager[E <: Entity[E], T <: AbstractTable[E]] {
+trait SlickEntityManager[E <: Entity[E], T <: AbstractTable[E]] extends EntityManager[E]{
 
   // ********** Management methods ********** //
   /** Initializes this manager. This is called once at the start of the application. */
@@ -16,19 +16,7 @@ trait SlickEntityManager[E <: Entity[E], T <: AbstractTable[E]] {
   def createTable(): Unit
   def tableName: String
 
-  // ********** Mutators ********** //
-  /** Persists a new entity (without ID) and returns the same entity with its ID in the database. */
-  def add(entity: E): E
-  /** Persists an update to an existing entity and returns the given entity. */
-  def update(entity: E): E
-  /** Deletes an existing entity from the database. */
-  def delete(entity: E): Unit
-
   // ********** Getters ********** //
-  /** Returns the entity with given ID or throws an exception. */
-  def findById(id: Long): E
-  /** Returns all stored entities. */
-  def fetchAll(): List[E]
   /**
     * Returns a new query that should be run by models.SlickUtils.dbRun. Don't run any mutating operations using these queries!
     *
