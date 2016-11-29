b9b97d6b05d22963a7fdf0660dadc5a74d19efd4
Jens Nyman <nymanjens.nj@gmail.com>
2016-11-29 11:39:25 +0000

    Small change to test file

--- app/js/src/main/scala/spatutorial/client/SPAMain.scala
+++ app/js/src/main/scala/spatutorial/client/SPAMain.scala
@@ -1,6 +1,8 @@
 package spatutorial.client
 
-import java.time.LocalDate
+import java.time.{LocalDate, LocalTime}
+import java.time.format.DateTimeFormatter
+import java.time.temporal.{ChronoField, TemporalField}
 
 import org.scalajs.dom
 
@@ -73,29 +75,38 @@ object SPAMain extends js.JSApp {
   import common._
   import common.JavaTimeImplicits._
   import java.time.Month._
+  import java.time.{Instant, LocalDate}
+  import java.time.format._
 
   @JSExport
   def factoPortabilityTest(): Unit = {
     out("factoPortabilityTest() starting")
     out(Clock.now)
-    out(TimeUtils.yearAt(Clock.now))
-    out(TimeUtils.yearAt(TimeUtils.instantAt(2100, DECEMBER, 2)))
-    TimeUtils.requireStartOfMonth(LocalDate.of(2010, NOVEMBER, 1))
-    out(TimeUtils.allMonths)
-    out(TimeUtils.instantAt(2100, DECEMBER, 1) < TimeUtils.instantAt(2100, DECEMBER, 2))
-    out(DatedMonth.containing(TimeUtils.instantAt(2100, DECEMBER, 1)).contains(TimeUtils.instantAt(2100, DECEMBER, 10)))
-    out(DatedMonth.allMonthsIn(2012))
 
-    val instant = TimeUtils.instantAt(2012, DECEMBER, 1)
-    implicit val i18n = new I18n {
-      override def apply(key: String, args: Any*): String = key
-    }
-    out(Formatting.formatDateTime(instant))
-    out(Formatting.formatDate(instant))
+    val instant = Instant.ofEpochMilli(100020303)
+    out(instant)
+    out(LocalDate.now)
+    out(LocalTime.now)
+    val time = LocalTime.now
 
-    out(MonthRange.forYear(2012) intersection MonthRange.atLeast(DatedMonth.containing(instant)))
-
-    X(null)
+//    out(TimeUtils.yearAt(Clock.now))
+//    out(TimeUtils.yearAt(TimeUtils.instantAt(2100, DECEMBER, 2)))
+//    TimeUtils.requireStartOfMonth(LocalDate.of(2010, NOVEMBER, 1))
+//    out(TimeUtils.allMonths)
+//    out(TimeUtils.instantAt(2100, DECEMBER, 1) < TimeUtils.instantAt(2100, DECEMBER, 2))
+//    out(DatedMonth.containing(TimeUtils.instantAt(2100, DECEMBER, 1)).contains(TimeUtils.instantAt(2100, DECEMBER, 10)))
+//    out(DatedMonth.allMonthsIn(2012))
+//
+//    val instant = TimeUtils.instantAt(2012, DECEMBER, 1)
+//    implicit val i18n = new I18n {
+//      override def apply(key: String, args: Any*): String = key
+//    }
+//    out(Formatting.formatDateTime(instant))
+//    out(Formatting.formatDate(instant))
+//
+//    out(MonthRange.forYear(2012) intersection MonthRange.atLeast(DatedMonth.containing(instant)))
+//
+//    X(null)
   }
 
   def out(x: Any): Unit = {
