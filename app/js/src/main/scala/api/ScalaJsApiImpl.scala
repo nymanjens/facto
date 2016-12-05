83ff2b8080663c8c9d4a1aa4c0848c87e05a38d9
Jens Nyman <nymanjens.nj@gmail.com>
2016-12-05 21:48:57 +0000

    Renamed ScalaJsApiImpl to _Server

---
+++ app/js/src/main/scala/api/ScalaJsApiImpl.scala
@@ -0,0 +1,9 @@
+package api
+
+//import models.accounting.config.Config
+//
+//final class ScalaJsApiServer extends ScalaJsApi {
+//  override def getAccountingConfig(): Config = accountingConfig
+//}
+//
+//object ScalaJsApiClient
--- app/jvm/src/main/scala/api/ScalaJsApiServer.scala
+++ app/jvm/src/main/scala/api/ScalaJsApiServer.scala
@@ -3,6 +3,6 @@ package api
 import com.google.inject._
 import models.accounting.config.Config
 
-private[api] final class ScalaJsApiImpl @Inject() (implicit accountingConfig: Config) extends ScalaJsApi {
+private[api] final class ScalaJsApiServer @Inject() (implicit accountingConfig: Config) extends ScalaJsApi {
   override def getAccountingConfig(): Config = accountingConfig
 }
