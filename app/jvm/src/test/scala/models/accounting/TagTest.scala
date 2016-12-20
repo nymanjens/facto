6d72f2beffeaf489e6bf979e9783c0a81e08b122
Jens Nyman <nymanjens.nj@gmail.com>
2016-12-20 17:44:23 +0000

    Re-enabled TagTest

--- app/jvm/src/test/scala/models/accounting/TagTest.scala
+++ app/jvm/src/test/scala/models/accounting/TagTest.scala
@@ -1,22 +1,26 @@
-// TODO: Fix this test
+package models.accounting
 
-//package models.accounting
-//
-//import org.specs2.mutable._
-//import play.api.test.WithApplication
-//
-//class TagTest extends Specification {
-//
-//  "isValidTagName" in new WithApplication {
-//    Tag.isValidTagName("") mustEqual false
-//    Tag.isValidTagName("'") mustEqual false
-//    Tag.isValidTagName("single-illegal-char-at-end?") mustEqual false
-//    Tag.isValidTagName("]single-illegal-char-at-start") mustEqual false
-//    Tag.isValidTagName("space in middle") mustEqual false
-//
-//    Tag.isValidTagName("a") mustEqual true
-//    Tag.isValidTagName("normal-string") mustEqual true
-//    Tag.isValidTagName("aC29_()_-_@_!") mustEqual true
-//    Tag.isValidTagName("aC29_()_-_@_!_&_$_+_=_._<>_;_:") mustEqual true
-//  }
-//}
+import org.specs2.mutable._
+import play.api.test.WithApplication
+
+class TagTest extends Specification {
+
+  "isValidTagName" in {
+    Tag.isValidTagName("") mustEqual false
+    Tag.isValidTagName("'") mustEqual false
+    Tag.isValidTagName("single-illegal-char-at-end?") mustEqual false
+    Tag.isValidTagName("]single-illegal-char-at-start") mustEqual false
+    Tag.isValidTagName("space in middle") mustEqual false
+
+    Tag.isValidTagName("a") mustEqual true
+    Tag.isValidTagName("normal-string") mustEqual true
+    Tag.isValidTagName("aC29_()_-_@_!") mustEqual true
+    Tag.isValidTagName("aC29_()_-_@_!_&_$_+_=_._<>_;_:") mustEqual true
+  }
+
+  "parseTagsString" in {
+    Tag.parseTagsString("a,b,c") mustEqual Seq(Tag("a"), Tag("b"), Tag("c"))
+    Tag.parseTagsString(",,b,c") mustEqual Seq(Tag("b"), Tag("c"))
+    Tag.parseTagsString("") mustEqual Seq()
+  }
+}
