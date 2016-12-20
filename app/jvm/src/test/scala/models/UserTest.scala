44374826d23f498705f57588a13c6416244f2d49
Jens Nyman <nymanjens.nj@gmail.com>
2016-12-20 14:38:01 +0000

    Fixed UserTest

--- app/jvm/src/test/scala/models/UserTest.scala
+++ app/jvm/src/test/scala/models/UserTest.scala
@@ -1,39 +1,37 @@
-// TODO: Fix this test
+package models
 
-//package models
-//
-//import com.google.inject._
-//import org.specs2.mutable._
-//import org.specs2.runner._
-//import org.junit.runner._
-//
-//import play.api.test._
-//
-//import models.SlickUtils.dbApi._
-//import common.testing.TestObjects._
-//import common.testing.TestUtils._
-//import common.testing._
-//import models.SlickUtils.dbRun
-//
-//@RunWith(classOf[JUnitRunner])
-//class UserTest extends HookedSpecification {
-//
-//  @Inject val userManager: User.Manager = null
-//
-//  override def before() = {
-//    Guice.createInjector(new FactoTestModule).injectMembers(this)
-//  }
-//
-//  "test the User model" in new WithApplication {
-//
-//    val user1 = userManager.add(userManager.newWithUnhashedPw(loginName = "alice", password = "j", name = "Alice"))
-//    userManager.fetchAll() mustEqual Seq(user1)
-//
-//    userManager.authenticate(loginName = "alice", password = "j") mustEqual true
-//    userManager.authenticate(loginName = "wrong_username", password = "j") mustEqual false
-//    userManager.authenticate(loginName = "alice", password = "wrong password") mustEqual false
-//
-//    userManager.findByLoginName(loginName = "alice") mustEqual Option(user1)
-//    userManager.findByLoginName(loginName = "wrong_username") mustEqual None
-//  }
-//}
+import com.google.inject._
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.test._
+
+import models.SlickUtils.dbApi._
+import common.testing.TestObjects._
+import common.testing.TestUtils._
+import common.testing._
+import models.SlickUtils.dbRun
+
+@RunWith(classOf[JUnitRunner])
+class UserTest extends HookedSpecification {
+
+  @Inject val userManager: SlickUserManager = null
+
+  override def before() = {
+    Guice.createInjector(new FactoTestModule).injectMembers(this)
+  }
+
+  "test the User model" in new WithApplication {
+
+    val user1 = userManager.add(SlickUserManager.createUser(loginName = "alice", password = "j", name = "Alice"))
+    userManager.fetchAll() mustEqual Seq(user1)
+
+    userManager.authenticate(loginName = "alice", password = "j") mustEqual true
+    userManager.authenticate(loginName = "wrong_username", password = "j") mustEqual false
+    userManager.authenticate(loginName = "alice", password = "wrong password") mustEqual false
+
+    userManager.findByLoginName(loginName = "alice") mustEqual Option(user1)
+    userManager.findByLoginName(loginName = "wrong_username") mustEqual None
+  }
+}
