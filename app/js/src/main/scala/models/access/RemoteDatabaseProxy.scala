267672e73aeeae3d7c57a3948d5a879227751152
Jens Nyman <nymanjens.nj@gmail.com>
2016-12-23 12:50:21 +0000

    Added RemoteDatabaseProxy API

--- app/js/src/main/scala/models/access/RemoteDatabaseProxy.scala
+++ app/js/src/main/scala/models/access/RemoteDatabaseProxy.scala
@@ -1,7 +1,31 @@
 package models.access
 
+import api.ScalaJsApi.EntityType
+
+import scala.concurrent.Future
 import api.ScalaJsApiClient
+import jsfacades.Loki
 
-final class RemoteDatabaseProxy(scalaJsApiClient: ScalaJsApiClient) {
+import scala.collection.immutable.Seq
 
+trait RemoteDatabaseProxy {
+
+  // **************** Getters ****************//
+  def newQuery(entityType: EntityType): Loki.ResultSet
+  /** Returns true if there are local pending modifications for the given entity. Note that only its id is used. */
+  def hasLocalModifications(entityType: EntityType)(entity: entityType.get): Boolean
+
+  // **************** Setters ****************//
+  def applyModifications(modifications: Seq[EntityModification]): Future[Unit]
+
+  // **************** Other ****************//
+  def registerListener(listener: RemoteDatabaseProxy.Listener): Unit
+}
+
+object RemoteDatabaseProxy {
+
+  trait Listener {
+    def addedLocally(entityModifications: Seq[EntityModification]): Unit
+    def persistedRemotely(entityModifications: Seq[EntityModification]): Unit
+  }
 }
