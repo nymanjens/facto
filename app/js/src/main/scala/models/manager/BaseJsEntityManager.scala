b17d663150a593b2c1fa79453e5829d643095ebf
Jens Nyman <nymanjens.nj@gmail.com>
2016-12-28 14:13:27 +0000

    Removed useless code

--- app/js/src/main/scala/models/manager/BaseJsEntityManager.scala
+++ app/js/src/main/scala/models/manager/BaseJsEntityManager.scala
@@ -9,22 +9,6 @@ import scala.util.Random
 abstract class BaseJsEntityManager[E <: Entity : EntityType](implicit database: RemoteDatabaseProxy) extends EntityManager[E] {
 
   // **************** Implementation of EntityManager ****************//
-  // TODO: Remove these
-  final def add(entity: E): E = {
-    require(entity.idOption.isEmpty, entity)
-
-    val id = Random.nextLong
-    val entityWithId = entity.withId(id).asInstanceOf[E]
-    database.persistModifications(Seq(EntityModification.Add(entityWithId)))
-    entityWithId
-  }
-
-  final def delete(entity: E): Unit = {
-    require(entity.idOption.isDefined, entity)
-
-    database.persistModifications(Seq(EntityModification.Remove[E](entity.id)))
-  }
-
   override final def findById(id: Long): E = {
     database.newQuery().findOne("id" -> id.toString).get
   }
