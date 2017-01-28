4711f4bf844e0a403d686c7833234c815222f194
Jens Nyman <nymanjens.nj@gmail.com>
2017-01-27 16:08:45 -0800

    Implemented Panel component

--- app/js/src/main/scala/components/Everything.scala
+++ app/js/src/main/scala/components/Everything.scala
@@ -2,7 +2,9 @@ package components
 
 import scala.collection.immutable.Seq
 import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.TagMod
 import japgolly.scalajs.react.vdom.prefix_<^._
+import components.ReactVdomUtils.^^
 import spatutorial.client.spacomponents.Bootstrap.{Button, CommonStyle}
 import spatutorial.shared._
 import stores.{EntriesStore, LastNEntriesStoreFactory}
@@ -10,19 +12,34 @@ import stores.LastNEntriesStoreFactory.{LastNEntriesState, N}
 
 import scalacss.ScalaCssReact._
 
+object ReactVdomUtils {
+  object ^^ {
+    def classes(cls: String*): TagMod = classes(cls.toVector)
+    def classes(cls: Seq[String]): TagMod = ^.classSetM(cls.map(c => (c, true)).toMap)
+  }
+}
+
 final class Everything(implicit entriesStoreFactory: LastNEntriesStoreFactory) {
 
   object Panel {
-    private case class Props(title: String, panelClasses: String)
+    private case class Props(title: String, panelClasses: Seq[String])
     private val component = ReactComponentB[Props]("Panel")
-      .renderPC((_, p, c) =>
-        <.div(
-          <.div(p.title),
-          <.div(c)
+      .renderPC((_, props, children) =>
+        <.div(^^.classes(Seq("row", "add-toc-level-1") ++ props.panelClasses),
+          <.div(^^.classes("col-lg-12"),
+            <.div(^^.classes("panel panel-default"),
+              <.div(^^.classes("panel-heading toc-title"),
+                props.title
+              ),
+              <.div(^^.classes("panel-body"),
+                children
+              )
+            )
+          )
         )
       ).build
 
-    def apply(title: String, panelClasses: String = "")(children: ReactNode*): ReactElement = {
+    def apply(title: String, panelClasses: Seq[String] = Seq())(children: ReactNode*): ReactElement = {
       component(Props(title, panelClasses), children: _*)
     }
   }
