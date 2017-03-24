8fba9e1aa4e2cea77edaed24603fe63a5db68936
Jens Nyman <nymanjens.nj@gmail.com>
2017-03-24 17:43:36 +0000

    TransactionGroupForm: Add button now works

--- app/js/src/main/scala/flux/react/app/transactiongroupform/TransactionGroupForm.scala
+++ app/js/src/main/scala/flux/react/app/transactiongroupform/TransactionGroupForm.scala
@@ -1,7 +1,7 @@
 package flux.react.app.transactiongroupform
 
 import common.Formatting._
-import common.I18n
+import common.{I18n, LoggingUtils}
 import common.time.Clock
 import flux.react.uielements
 import flux.react.uielements.EntriesListTable.NumEntriesStrategy
@@ -20,16 +20,40 @@ final class TransactionGroupForm(implicit accountingConfig: Config,
                                  exchangeRateManager: ExchangeRateManager,
                                  i18n: I18n) {
 
-  private val component = ReactComponentB[Unit](getClass.getSimpleName)
-    .renderPS((_, props, state) =>
+  private val component = ReactComponentB[Props](getClass.getSimpleName)
+    .initialState[State](State(numberOfTransactionPanels = 2))
+    .renderBackend[Backend]
+    .build
+
+  // **************** API ****************//
+  def apply(): ReactElement = {
+    component(Props())
+  }
+
+  // **************** Private inner types ****************//
+  private case class State(numberOfTransactionPanels: Int) {
+    def plusPanel(): State = copy(numberOfTransactionPanels = numberOfTransactionPanels + 1)
+    def minusPanel(): State = copy(numberOfTransactionPanels = numberOfTransactionPanels + 1)
+  }
+
+  private case class Props()
+
+  private final class Backend(val $: BackendScope[Props, State]) {
+
+    def render(props: Props, state: State) = LoggingUtils.logExceptions {
       <.div(
         ^.className := "transaction-group-form",
-        TransactionPanel(),
-        AddTransactionPanel(onClick = Callback())
+        for (i <- 0 until state.numberOfTransactionPanels) yield {
+          TransactionPanel()
+        },
+        AddTransactionPanel(onClick = addTransactionPanel())
       )
-    ).build
+    }
 
-  def apply(): ReactElement = {
-    component()
+    private def addTransactionPanel(): Callback = Callback {
+      LoggingUtils.logExceptions {
+        $.modState(_.plusPanel()).runNow()
+      }
+    }
   }
 }
