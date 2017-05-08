748df3eb1469f059b9cba2ed8a09857cc5bbff87
Jens Nyman <nymanjens.nj@gmail.com>
2017-05-08 11:08:10 +0100

    Add form state: totalFlow, totalFlowExceptLast: ReferenceMoney

--- app/js/src/main/scala/flux/react/app/transactiongroupform/TransactionGroupForm.scala
+++ app/js/src/main/scala/flux/react/app/transactiongroupform/TransactionGroupForm.scala
@@ -1,18 +1,25 @@
 package flux.react.app.transactiongroupform
 
-import common.LoggingUtils.{logExceptions, LogExceptionsCallback}
 import common.I18n
+import common.LoggingUtils.{LogExceptionsCallback, logExceptions}
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
+import models.accounting.money.{ExchangeRateManager, ReferenceMoney}
 
 import scala.collection.immutable.Seq
 
 final class TransactionGroupForm(implicit i18n: I18n,
+                                 exchangeRateManager: ExchangeRateManager,
                                  transactionPanel: TransactionPanel,
                                  addTransactionPanel: AddTransactionPanel) {
 
   private val component = ReactComponentB[Props](getClass.getSimpleName)
-    .initialState[State](State(panelIndices = Seq(0, 1)))
+    .initialState[State](
+    State(
+      // TODO: Update these when updating an existing TransactionGroup.
+      panelIndices = Seq(0, 1),
+      totalFlow = ReferenceMoney(0),
+      totalFlowExceptLast = ReferenceMoney(0)))
     .renderBackend[Backend]
     .build
 
@@ -25,7 +32,9 @@ final class TransactionGroupForm(implicit i18n: I18n,
   private def panelRef(panelIndex: Int): transactionPanel.Reference = transactionPanel.ref(s"panel_$panelIndex")
 
   // **************** Private inner types ****************//
-  private case class State(panelIndices: Seq[Int]) {
+  private case class State(panelIndices: Seq[Int],
+                           totalFlow: ReferenceMoney,
+                           totalFlowExceptLast: ReferenceMoney) {
     def plusPanel(): State = copy(panelIndices = panelIndices :+ panelIndices.max + 1)
     def minusPanelIndex(index: Int): State = copy(panelIndices = panelIndices.filter(_ != index))
   }
@@ -36,21 +45,25 @@ final class TransactionGroupForm(implicit i18n: I18n,
 
     def render(props: Props, state: State) = logExceptions {
       <.div(
-        ^.className := "transaction-group-form",
-        for ((panelIndex, i) <- state.panelIndices.zipWithIndex) yield {
-          val firstPanel = state.panelIndices.head
-          transactionPanel(
-            key = panelIndex,
-            ref = panelRef(panelIndex),
-            title = i18n("facto.transaction") + " " + (i + 1),
-            defaultPanel = if (panelIndex == firstPanel) None else Some(panelRef(panelIndex = firstPanel)($)),
-            closeButtonCallback = if (panelIndex == firstPanel) None else Some(removeTransactionPanel(panelIndex)),
-            onFormChange = () => {
-              println("onFormChange(): flowValueOrDefault = " + panelRef(panelIndex)($).flowValueOrDefault)
-            }
-          )
-        },
-        addTransactionPanel(onClick = addTransactionPanelCallback())
+        <.div(
+          state.totalFlow.toString,
+          state.totalFlowExceptLast.toString
+        ),
+        <.div(
+          ^.className := "transaction-group-form",
+          for ((panelIndex, i) <- state.panelIndices.zipWithIndex) yield {
+            val firstPanel = state.panelIndices.head
+            transactionPanel(
+              key = panelIndex,
+              ref = panelRef(panelIndex),
+              title = i18n("facto.transaction") + " " + (i + 1),
+              defaultPanel = if (panelIndex == firstPanel) None else Some(panelRef(panelIndex = firstPanel)($)),
+              closeButtonCallback = if (panelIndex == firstPanel) None else Some(removeTransactionPanel(panelIndex)),
+              onFormChange = this.onFormChange
+            )
+          },
+          addTransactionPanel(onClick = addTransactionPanelCallback())
+        )
       )
     }
 
@@ -60,6 +73,18 @@ final class TransactionGroupForm(implicit i18n: I18n,
 
     private def removeTransactionPanel(index: Int): Callback = LogExceptionsCallback {
       $.modState(_.minusPanelIndex(index)).runNow()
+    }
+
+    private def onFormChange(): Unit = {
+      val state = $.state.runNow()
+      val flows = for (panelIndex <- state.panelIndices) yield {
+        val datedMoney = panelRef(panelIndex)($).flowValueOrDefault
+        datedMoney.exchangedForReferenceCurrency
+      }
+
+      $.modState(_.copy(
+        totalFlow = flows.sum,
+        totalFlowExceptLast = flows.takeRight(1).sum)).runNow()
     }
   }
 }
