24c47416f6349728a99a4c740a15780ba727848b
Jens Nyman <nymanjens.nj@gmail.com>
2017-07-06 22:22:40 +0100

    Add EndowmentEntriesStoreFactory

---
+++ app/js/src/main/scala/flux/stores/EndowmentEntriesStoreFactory.scala
@@ -0,0 +1,50 @@
+package flux.stores
+
+import flux.stores.entries.GeneralEntry
+import jsfacades.Loki
+import models.access.RemoteDatabaseProxy
+import models.accounting.Transaction
+import models.accounting.config.{Account, Config}
+import models.manager.{EntityModification, EntityType}
+
+import scala.collection.immutable.Seq
+
+final class EndowmentEntriesStoreFactory(implicit database: RemoteDatabaseProxy, accountingConfig: Config)
+    extends EntriesListStoreFactory[GeneralEntry, Account] {
+
+  override protected def createNew(maxNumEntries: Int, account: Account) = new Store {
+    override protected def calculateState() = {
+      val transactions: Seq[Transaction] =
+        database
+          .newQuery[Transaction]()
+          .find(
+            "categoryCode" -> accountingConfig.constants.endowmentCategory.code,
+            "beneficiaryAccountCode" -> account.code)
+          .sort(
+            Loki.Sorting
+              .by("consumedDate")
+              .desc()
+              .thenBy("createdDate")
+              .desc()
+              .thenBy("id")
+              .desc())
+          .limit(3 * maxNumEntries)
+          .data()
+          .reverse
+
+      var entries = transactions.map(t => GeneralEntry(Seq(t)))
+
+      entries = GeneralEntry.combineConsecutiveOfSameGroup(entries)
+
+      EntriesListStoreFactory.State(entries.takeRight(maxNumEntries), hasMore = entries.size > maxNumEntries)
+    }
+
+    override protected def modificationImpactsState(entityModification: EntityModification,
+                                                    state: State): Boolean = {
+      entityModification.entityType == EntityType.TransactionType
+    }
+  }
+
+  def get(account: Account, maxNumEntries: Int): Store =
+    get(Input(maxNumEntries = maxNumEntries, additionalInput = account))
+}
