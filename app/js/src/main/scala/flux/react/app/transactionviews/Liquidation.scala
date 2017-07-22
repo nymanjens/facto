de35257769614fd995b5c29be3a277f09e242805
Jens Nyman <nymanjens.nj@gmail.com>
2017-07-22 08:37:09 +0100

    Add liquidationRepayButton

--- app/js/src/main/scala/flux/react/app/transactionviews/Liquidation.scala
+++ app/js/src/main/scala/flux/react/app/transactionviews/Liquidation.scala
@@ -3,15 +3,16 @@ package flux.react.app.transactionviews
 import common.Formatting._
 import common.I18n
 import common.time.Clock
+import common.LoggingUtils.LogExceptionsCallback
 import flux.react.app.transactionviews.EntriesListTable.NumEntriesStrategy
 import flux.react.router.Page
 import flux.react.uielements
-import flux.stores.entries.{AccountPair, GeneralEntry, LiquidationEntriesStoreFactory, LiquidationEntry}
+import flux.stores.entries.{AccountPair, LiquidationEntriesStoreFactory, LiquidationEntry}
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.extra.router.RouterCtl
 import japgolly.scalajs.react.vdom.html_<^._
 import models.accounting.config.{Account, Config}
-import models.accounting.money.ExchangeRateManager
+import models.accounting.money.{ExchangeRateManager, ReferenceMoney}
 import models.{EntityAccess, User}
 
 import scala.collection.immutable.Seq
@@ -35,12 +36,15 @@ final class Liquidation(implicit entriesStoreFactory: LiquidationEntriesStoreFac
               (account2, i2) <- accountingConfig.personallySortedAccounts.zipWithIndex
               if i1 < i2
             } yield {
+              val accountPair = AccountPair(account1, account2)
+              val startNumEntries = 10
               EntriesListTable[LiquidationEntry, AccountPair](
                 tableTitle = i18n("facto.debt-of", account1.longName, account2.longName),
                 tableClasses = Seq("table-liquidation"),
                 key = s"${account1.code}_${account2.code}",
-                numEntriesStrategy = NumEntriesStrategy(start = 10, intermediateBeforeInf = Seq(30)),
-                props = AccountPair(account1, account2),
+                numEntriesStrategy =
+                  NumEntriesStrategy(start = startNumEntries, intermediateBeforeInf = Seq(30)),
+                props = accountPair,
                 tableHeaders = Seq(
                   <.th(i18n("facto.payed")),
                   <.th(i18n("facto.beneficiary")),
@@ -49,7 +53,17 @@ final class Liquidation(implicit entriesStoreFactory: LiquidationEntriesStoreFac
                   <.th(i18n("facto.description")),
                   <.th(i18n("facto.flow")),
                   <.th(s"${account1.veryShortName} -> ${account2.veryShortName}"),
-                  <.th("")
+                  <.th(repayButton(
+                    account1 = account1,
+                    account2 = account2,
+                    amount = entriesStoreFactory
+                      .get(accountPair = accountPair, maxNumEntries = startNumEntries)
+                      .state
+                      .entries
+                      .lastOption
+                      .map(_.debt) getOrElse ReferenceMoney(0),
+                    router = props.router
+                  ))
                 ),
                 calculateTableData = entry =>
                   Seq[VdomElement](
@@ -72,6 +86,29 @@ final class Liquidation(implicit entriesStoreFactory: LiquidationEntriesStoreFac
   // **************** API ****************//
   def apply(router: RouterCtl[Page]): VdomElement = {
     component(Props(router))
+  }
+
+  // **************** Private helper methods ****************//
+  private def repayButton(account1: Account,
+                          account2: Account,
+                          amount: ReferenceMoney,
+                          router: RouterCtl[Page]): VdomElement = {
+    <.a(
+      ^.className := "btn btn-info btn-xs",
+      ^.onClick --> LogExceptionsCallback {
+        router
+          .set(
+            Page.NewForRepaymentPage(
+              accountCode1 = account1.code,
+              accountCode2 = account2.code,
+              amountInCents = amount.cents))
+          .runNow()
+      },
+      ^.role := "button",
+      <.i(^.className := "fa fa-check-square-o fa-fw"),
+      i18n("facto.repay")
+    )
+
   }
 
   // **************** Private inner types ****************//
