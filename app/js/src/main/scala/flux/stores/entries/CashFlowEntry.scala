bbba175eb44326df2002468a95baca6b6b823394
Jens Nyman <nymanjens.nj@gmail.com>
2017-07-24 08:10:50 +0100

    Add empty CashFlowEntriesStoreFactory

---
+++ app/js/src/main/scala/flux/stores/entries/CashFlowEntry.scala
@@ -0,0 +1,25 @@
+package flux.stores.entries
+
+import models.accounting.{Transaction, _}
+import models.accounting.money.{DatedMoney, MoneyWithGeneralCurrency}
+
+import scala.collection.immutable.Seq
+
+sealed trait CashFlowEntry
+
+object CashFlowEntry {
+
+  case class RegularEntry(override val transactions: Seq[Transaction],
+                          private val nonDatedBalance: MoneyWithGeneralCurrency,
+                          balanceVerified: Boolean)
+      extends GroupedTransactions(transactions)
+      with CashFlowEntry {
+
+    def balance: DatedMoney = {
+      val latestDate = transactions.map(_.transactionDate).max
+      nonDatedBalance.withDate(latestDate)
+    }
+  }
+
+  case class BalanceCorrection(balanceCheck: BalanceCheck) extends CashFlowEntry
+}
