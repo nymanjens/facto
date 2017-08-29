2dbd44d6c0fb515777d30ed66d0dec72f0a5a170
Jens Nyman <nymanjens.nj@gmail.com>
2017-08-29 07:07:37 +0100

    Minimised Page visibility

--- app/js/src/main/scala/flux/react/router/Page.scala
+++ app/js/src/main/scala/flux/react/router/Page.scala
@@ -9,8 +9,7 @@ import org.scalajs.dom
 sealed trait Page
 object Page {
 
-  trait HasReturnTo {
-    def returnToWithoutPrefix: String
+  abstract class HasReturnTo(private val returnToWithoutPrefix: String) {
     def returnToPath: Path = Path(RouterFactory.pathPrefix + returnToWithoutPrefix)
   }
   object HasReturnTo {
@@ -26,50 +25,50 @@ object Page {
   case object EndowmentsPage extends Page
 
   // Accounting forms - transactions
-  case class NewTransactionGroupPage(override val returnToWithoutPrefix: String)
-      extends Page
-      with HasReturnTo
+  case class NewTransactionGroupPage private (returnToWithoutPrefix: String)
+      extends HasReturnTo(returnToWithoutPrefix)
+      with Page
   object NewTransactionGroupPage {
     def apply(): NewTransactionGroupPage = NewTransactionGroupPage(HasReturnTo.getDomPathWithoutPrefix)
   }
-  case class EditTransactionGroupPage(transactionGroupId: Long, override val returnToWithoutPrefix: String)
-      extends Page
-      with HasReturnTo
+  case class EditTransactionGroupPage private (transactionGroupId: Long, returnToWithoutPrefix: String)
+      extends HasReturnTo(returnToWithoutPrefix)
+      with Page
   object EditTransactionGroupPage {
     // Note: Getting ID here rather than TransactionGroup because we may not have fetched the TransactionGroup.
     def apply(transactionGroupId: Long): EditTransactionGroupPage =
       EditTransactionGroupPage(transactionGroupId, HasReturnTo.getDomPathWithoutPrefix)
   }
-  case class NewFromTemplatePage(templateCode: String, override val returnToWithoutPrefix: String)
-      extends Page
-      with HasReturnTo
+  case class NewFromTemplatePage private (templateCode: String, returnToWithoutPrefix: String)
+      extends HasReturnTo(returnToWithoutPrefix)
+      with Page
   object NewFromTemplatePage {
     def apply(template: Template): NewFromTemplatePage =
       NewFromTemplatePage(template.code, HasReturnTo.getDomPathWithoutPrefix)
   }
-  case class NewForRepaymentPage(accountCode1: String,
-                                 accountCode2: String,
-                                 amountInCents: Long,
-                                 override val returnToWithoutPrefix: String)
-      extends Page
-      with HasReturnTo
+  case class NewForRepaymentPage private (accountCode1: String,
+                                          accountCode2: String,
+                                          amountInCents: Long,
+                                          returnToWithoutPrefix: String)
+      extends HasReturnTo(returnToWithoutPrefix)
+      with Page
   object NewForRepaymentPage {
     def apply(account1: Account, account2: Account, amount: ReferenceMoney): NewForRepaymentPage =
       NewForRepaymentPage(account1.code, account2.code, amount.cents, HasReturnTo.getDomPathWithoutPrefix)
   }
 
   // Accounting forms - balance checks
-  case class NewBalanceCheckPage private (reservoirCode: String, override val returnToWithoutPrefix: String)
-      extends Page
-      with HasReturnTo
+  case class NewBalanceCheckPage private (reservoirCode: String, returnToWithoutPrefix: String)
+      extends HasReturnTo(returnToWithoutPrefix)
+      with Page
   object NewBalanceCheckPage {
     def apply(reservoir: MoneyReservoir): NewBalanceCheckPage =
       NewBalanceCheckPage(reservoir.code, HasReturnTo.getDomPathWithoutPrefix)
   }
 
-  case class EditBalanceCheckPage private (balanceCheckId: Long, override val returnToWithoutPrefix: String)
-      extends Page
-      with HasReturnTo
+  case class EditBalanceCheckPage private (balanceCheckId: Long, returnToWithoutPrefix: String)
+      extends HasReturnTo(returnToWithoutPrefix)
+      with Page
   object EditBalanceCheckPage {
     def apply(balanceCheck: BalanceCheck): EditBalanceCheckPage =
       EditBalanceCheckPage(balanceCheck.id, HasReturnTo.getDomPathWithoutPrefix)
