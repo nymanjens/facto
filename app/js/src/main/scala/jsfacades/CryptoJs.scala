3607c2185c4dc786de505381ecd92e3c99512fa3
Jens Nyman <nymanjens.nj@gmail.com>
2017-09-10 14:32:53 +0100

    Add CryptoJs facade

---
+++ app/js/src/main/scala/jsfacades/CryptoJs.scala
@@ -0,0 +1,33 @@
+package jsfacades
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+import scala2js.Converters._
+
+object CryptoJs {
+
+  @JSGlobal("CryptoJS.AES")
+  @js.native
+  object AES extends js.Object {
+    def encrypt(stringToEncrypt: String, password: String): EncryptResult = js.native
+    def decrypt(stringToDecrypt: String, password: String): DecryptResult = js.native
+  }
+
+  @js.native
+  trait EncryptResult extends js.Object {
+    override def toString(): String = js.native
+  }
+
+  @js.native
+  trait DecryptResult extends js.Object {
+    def toString(encoding: Encoding): String = js.native
+  }
+
+  @js.native
+  trait Encoding extends js.Object
+  object Encoding {
+    @JSGlobal("CryptoJS.enc.Utf8")
+    @js.native
+    object Utf8 extends Encoding
+  }
+}
