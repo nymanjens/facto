ecf40d2836170dfa8936f799add5bca12e15e912
Jens Nyman <nymanjens.nj@gmail.com>
2017-09-22 10:07:13 +0100

    Add ComplexQueryStore.parsePrefixAndSuffix()

--- app/js/src/main/scala/flux/stores/ComplexQueryStore.scala
+++ app/js/src/main/scala/flux/stores/ComplexQueryStore.scala
@@ -1,7 +1,8 @@
 package flux.stores
 
+import common.GuavaReplacement.Splitter
 import common.ScalaUtils.visibleForTesting
-import flux.stores.ComplexQueryStore.{QueryFilter, QueryPart}
+import flux.stores.ComplexQueryStore.{CombinedQueryFilter, Prefix, QueryFilter, QueryPart}
 import jsfacades.LokiJs
 import jsfacades.LokiJs.ResultSet
 import models.access.RemoteDatabaseProxy
@@ -18,11 +19,32 @@ final class ComplexQueryStore(implicit database: RemoteDatabaseProxy) {
   }
 
   // **************** Private helper methods **************** //
-  private def parseQuery(query: String): QueryFilter = {
-    ???
-//    QueryFilter.Combined {
-//      splitInParts(query) map ???
-//    }
+  private def parseQuery(query: String): CombinedQueryFilter = {
+    CombinedQueryFilter {
+      splitInParts(query) map {
+        case QueryPart(string, negated) =>
+          val prefixAndSuffix = parsePrefixAndSuffix(string)
+
+          string match {
+            case _ => ???
+          }
+      }
+    }
+  }
+  @visibleForTesting private[stores] def parsePrefixAndSuffix(string: String): Option[(Prefix, String)] = {
+    val prefixStringToPrefix: Map[String, Prefix] = {
+      for {
+        prefix <- Prefix.all
+        prefixString <- prefix.prefixStrings
+      } yield prefixString -> prefix
+    }.toMap
+
+    val split = Splitter.on(':').split(string).toList
+    split match {
+      case prefix :: suffix if (prefixStringToPrefix contains prefix) && suffix.mkString(":").nonEmpty =>
+        Some((prefixStringToPrefix(prefix), suffix.mkString(":")))
+      case _ => None
+    }
   }
   @visibleForTesting private[stores] def splitInParts(query: String): Seq[QueryPart] = {
     val quotes = Seq('"', '\'')
@@ -68,6 +90,13 @@ object ComplexQueryStore {
     def applyWithFunction(resultSet: LokiJs.ResultSet[Transaction], invert: Boolean = false): Unit = {}
   }
 
+  private case class CombinedQueryFilter(delegates: Seq[QueryFilter]) {
+    def apply(resultSet: ResultSet[Transaction]): Unit = {
+      delegates.foreach(_.applyWithOperator(resultSet))
+      delegates.foreach(_.applyWithFunction(resultSet))
+    }
+  }
+
   private object QueryFilter {
     case class Negated(delegate: QueryFilter) extends QueryFilter {
       override def applyWithOperator(resultSet: ResultSet[Transaction], invert: Boolean) =
@@ -75,16 +104,28 @@ object ComplexQueryStore {
       override def applyWithFunction(resultSet: ResultSet[Transaction], invert: Boolean) =
         delegate.applyWithFunction(resultSet, invert = !invert)
     }
-    case class Combined(delegates: Seq[QueryFilter]) extends QueryFilter {
-      override def applyWithOperator(resultSet: ResultSet[Transaction], invert: Boolean) =
-        delegates.foreach(_.applyWithOperator(resultSet, invert = invert))
-      override def applyWithFunction(resultSet: ResultSet[Transaction], invert: Boolean) =
-        delegates.foreach(_.applyWithFunction(resultSet, invert = invert))
-    }
   }
 
   @visibleForTesting private[stores] case class QueryPart(unquotedString: String, negated: Boolean = false)
   @visibleForTesting private[stores] object QueryPart {
     def not(unquotedString: String): QueryPart = QueryPart(unquotedString, negated = true)
+  }
+
+  @visibleForTesting private[stores] sealed case class Prefix(prefixStrings: Seq[String]) {
+    override def toString = getClass.getSimpleName
+  }
+  @visibleForTesting private[stores] object Prefix {
+    def all: Seq[Prefix] =
+      Seq(Issuer, Beneficiary, Reservoir, Category, Description, Flow, Detail, Tag, Date)
+
+    object Issuer extends Prefix(Seq("issuer", "i"))
+    object Beneficiary extends Prefix(Seq("beneficiary", "b"))
+    object Reservoir extends Prefix(Seq("reservoir", "r"))
+    object Category extends Prefix(Seq("category", "c"))
+    object Description extends Prefix(Seq("description"))
+    object Flow extends Prefix(Seq("flow", "amount", "a"))
+    object Detail extends Prefix(Seq("detail"))
+    object Tag extends Prefix(Seq("tag", "t"))
+    object Date extends Prefix(Seq("date", "d"))
   }
 }
--- app/js/src/test/scala/flux/stores/ComplexQueryStoreTest.scala
+++ app/js/src/test/scala/flux/stores/ComplexQueryStoreTest.scala
@@ -4,7 +4,7 @@ import utest._
 
 import scala.collection.immutable.Seq
 import scala2js.Converters._
-import flux.stores.ComplexQueryStore.QueryPart
+import flux.stores.ComplexQueryStore.{Prefix, QueryPart}
 
 object ComplexQueryStoreTest extends TestSuite {
 
@@ -15,6 +15,42 @@ object ComplexQueryStoreTest extends TestSuite {
     implicit val fakeDatabase = testModule.fakeRemoteDatabaseProxy
 
     val complexQueryStore = new ComplexQueryStore()
+
+    "parsePrefixAndSuffix()" - {
+      "single colon" - {
+        for {
+          prefix <- Prefix.all
+          prefixString <- prefix.prefixStrings
+        } {
+          complexQueryStore.parsePrefixAndSuffix(s"$prefixString:some value") ==>
+            Some((prefix, "some value"))
+        }
+      }
+      "multiple colons" - {
+        for {
+          prefix <- Prefix.all
+          prefixString <- prefix.prefixStrings
+        } {
+          complexQueryStore
+            .parsePrefixAndSuffix(s"$prefixString:some: value") ==> Some((prefix, "some: value"))
+        }
+      }
+      "wrong prefix" - {
+        complexQueryStore.parsePrefixAndSuffix(s"unknownPrefix:some: value") ==> None
+      }
+      "empty string" - {
+        complexQueryStore.parsePrefixAndSuffix(s"") ==> None
+      }
+      "empty prefix" - {
+        complexQueryStore.parsePrefixAndSuffix(s":some value") ==> None
+      }
+      "empty suffix" - {
+        complexQueryStore.parsePrefixAndSuffix(s"category:") ==> None
+      }
+      "no colons" - {
+        complexQueryStore.parsePrefixAndSuffix(s"category") ==> None
+      }
+    }
 
     "splitInParts()" - {
       "empty string" - {
