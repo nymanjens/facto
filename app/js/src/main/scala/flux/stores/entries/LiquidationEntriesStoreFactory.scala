edbdd4a7be72e714fd44a3d5000980e1f6bba64a
Jens Nyman <nymanjens.nj@gmail.com>
2017-09-30 21:39:11 +0100

    Make LiquidationEntriesStoreFactory more efficient

--- app/js/src/main/scala/flux/stores/entries/LiquidationEntriesStoreFactory.scala
+++ app/js/src/main/scala/flux/stores/entries/LiquidationEntriesStoreFactory.scala
@@ -1,14 +1,17 @@
 package flux.stores.entries
 
+import common.LoggingUtils.logExceptions
+import common.GuavaReplacement.Stopwatch
 import jsfacades.LokiJs
 import models.EntityAccess
 import models.access.RemoteDatabaseProxy
-import models.accounting.{BalanceCheck, Transaction}
-import models.accounting.config.{Account, Config}
+import models.accounting.Transaction
+import models.accounting.config.{Account, Config, MoneyReservoir}
 import models.accounting.money.{ExchangeRateManager, ReferenceMoney}
 
 import scala.collection.immutable.Seq
 import scala2js.Keys
+import scala2js.Converters._
 
 final class LiquidationEntriesStoreFactory(implicit database: RemoteDatabaseProxy,
                                            accountingConfig: Config,
@@ -18,17 +21,34 @@ final class LiquidationEntriesStoreFactory(implicit database: RemoteDatabaseProx
 
   override protected def createNew(maxNumEntries: Int, accountPair: AccountPair) =
     new TransactionsListStore[LiquidationEntry] {
-      override protected def calculateState() = {
-        val allTransactions: Seq[Transaction] =
-          database
-            .newQuery[Transaction]()
-            .sort(
-              LokiJs.Sorting
-                .ascBy(Keys.Transaction.transactionDate)
-                .thenAscBy(Keys.Transaction.createdDate)
-                .thenAscBy(Keys.id))
-            .data()
+      override protected def calculateState() = logExceptions {
+        val stopwatch = Stopwatch.createStarted()
+        val result = calculatePart2(getTransactions1_original())
+//        require(calculatePart2(getTransactions2_multipleQueries()) == result)
+        require(calculatePart2(getTransactions3_complexQuery()) == result)
 
+        def time(name: String)(func: => Seq[Transaction]): Unit = {
+          println(s"  Calculating for $accountPair ($name)")
+          val stopwatch = Stopwatch.createStarted()
+          for (_ <- 1 to 10) {
+            val result = func
+            println(s"Got ${result.length} rows")
+          }
+          println(s"  Done in ${stopwatch.elapsed.toMillis / 1e3} seconds")
+        }
+
+        for (_ <- 1 to 2) {
+          time("1 - original")(getTransactions1_original())
+          time("2 - different requests")(getTransactions2_multipleQueries())
+          time("3 - single complex filter")(getTransactions3_complexQuery())
+          println("")
+        }
+          println("")
+
+        result
+      }
+
+      private def calculatePart2(allTransactions: Seq[Transaction]) = {
         val relevantTransactions =
           for {
             transaction <- allTransactions
@@ -59,6 +79,85 @@ final class LiquidationEntriesStoreFactory(implicit database: RemoteDatabaseProx
         EntriesListStoreFactory.State(
           entries.takeRight(maxNumEntries),
           hasMore = entries.size > maxNumEntries)
+      }
+
+      private def getTransactions1_original() = {
+        database
+          .newQuery[Transaction]()
+          .sort(
+            LokiJs.Sorting
+              .ascBy(Keys.Transaction.transactionDate)
+              .thenAscBy(Keys.Transaction.createdDate)
+              .thenAscBy(Keys.id))
+          .data()
+      }
+
+      private def getTransactions2_multipleQueries() = {
+        def dataWithFilter(
+            filter: LokiJs.ResultSet[Transaction] => LokiJs.ResultSet[Transaction]): Seq[Transaction] = {
+          filter(database.newQuery[Transaction]())
+            .sort(
+              LokiJs.Sorting
+                .ascBy(Keys.Transaction.transactionDate)
+                .thenAscBy(Keys.Transaction.createdDate)
+                .thenAscBy(Keys.id))
+            .data()
+        }
+        def reservoirsOwnedBy(account: Account): Seq[MoneyReservoir] = {
+          accountingConfig.moneyReservoirs(includeHidden = true).filter(r => r.owner == account)
+        }
+
+        Seq(
+          dataWithFilter(
+            _.filterAnyOf(
+              Keys.Transaction.moneyReservoirCode,
+              reservoirsOwnedBy(accountPair.account1).map(_.code))
+              .filter(Keys.Transaction.beneficiaryAccountCode, accountPair.account2.code)),
+          dataWithFilter(
+            _.filterAnyOf(
+              Keys.Transaction.moneyReservoirCode,
+              reservoirsOwnedBy(accountPair.account2).map(_.code))
+              .filter(Keys.Transaction.beneficiaryAccountCode, accountPair.account1.code)),
+          dataWithFilter(
+            _.filter(Keys.Transaction.moneyReservoirCode, "")
+              .filterAnyOf(Keys.Transaction.beneficiaryAccountCode, accountPair.toSet.map(_.code).toVector))
+        ).flatten
+      }
+
+      private def getTransactions3_complexQuery() = {
+        def reservoirsOwnedBy(account: Account): Seq[MoneyReservoir] = {
+          accountingConfig.moneyReservoirs(includeHidden = true).filter(r => r.owner == account)
+        }
+
+        database
+          .newQuery[Transaction]()
+          .filter(LokiJs.ResultSet.Filter.or(
+            LokiJs.ResultSet.Filter.and(
+              LokiJs.ResultSet.Filter.anyOf(
+                Keys.Transaction.moneyReservoirCode,
+                reservoirsOwnedBy(accountPair.account1).map(_.code)),
+              LokiJs.ResultSet.Filter
+                .equal(Keys.Transaction.beneficiaryAccountCode, accountPair.account2.code)
+            ),
+            LokiJs.ResultSet.Filter.and(
+              LokiJs.ResultSet.Filter.anyOf(
+                Keys.Transaction.moneyReservoirCode,
+                reservoirsOwnedBy(accountPair.account2).map(_.code)),
+              LokiJs.ResultSet.Filter
+                .equal(Keys.Transaction.beneficiaryAccountCode, accountPair.account1.code)
+            ),
+            LokiJs.ResultSet.Filter.and(
+              LokiJs.ResultSet.Filter.equal(Keys.Transaction.moneyReservoirCode, ""),
+              LokiJs.ResultSet.Filter
+                .anyOf(Keys.Transaction.beneficiaryAccountCode, accountPair.toSet.map(_.code).toVector)
+            )
+          ))
+          .sort(
+            LokiJs.Sorting
+              .ascBy(Keys.Transaction.transactionDate)
+              .thenAscBy(Keys.Transaction.createdDate)
+              .thenAscBy(Keys.id))
+          .data()
       }
 
       override protected def transactionUpsertImpactsState(transaction: Transaction, state: State) =
