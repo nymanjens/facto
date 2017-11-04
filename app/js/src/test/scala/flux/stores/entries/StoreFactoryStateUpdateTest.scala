492ece3a15b97e7dc6a8c9738ce79782c3a4de51
Jens Nyman <nymanjens.nj@gmail.com>
2017-11-04 14:53:27 +0000

    Revert "Revert "Add CashFlow factory to UpdateTest""
    This reverts commit 0c64905afb4a7771917d045cf4def8b582a00490.

--- app/js/src/test/scala/flux/stores/entries/StoreFactoryStateUpdateTest.scala
+++ app/js/src/test/scala/flux/stores/entries/StoreFactoryStateUpdateTest.scala
@@ -2,7 +2,7 @@ package flux.stores.entries
 
 import java.time.Month.JANUARY
 
-import common.testing.FakeRemoteDatabaseProxy
+import common.testing.{FakeRemoteDatabaseProxy, TestModule}
 import common.testing.TestObjects._
 import common.time.LocalDateTime
 import common.time.LocalDateTimes.createDateTime
@@ -16,8 +16,8 @@ import scala2js.Converters._
 object StoreFactoryStateUpdateTest extends TestSuite {
 
   override def tests = TestSuite {
-    implicit val database = new FakeRemoteDatabaseProxy()
-    val factory: AllEntriesStoreFactory = new AllEntriesStoreFactory()
+    val testModule = new ThisTestModule()
+    val database = testModule.fakeRemoteDatabaseProxy
 
     def runTest(store: EntriesStore[_], updatesWithImpact: ListMap[EntityModification, StateImpact]): Unit = {
       var lastState = store.state
@@ -26,8 +26,11 @@ object StoreFactoryStateUpdateTest extends TestSuite {
         database.persistModifications(update)
 
         stateImpact match {
-          case StateImpact.NoChange => store.state ==> lastState
-          case StateImpact.Change => assert(store.state != lastState)
+          case StateImpact.NoChange =>
+            store.state ==> lastState
+          case StateImpact.Change =>
+            val storeState = store.state
+            assert(storeState != lastState)
           case StateImpact.Undefined =>
         }
 
@@ -36,14 +39,14 @@ object StoreFactoryStateUpdateTest extends TestSuite {
     }
 
     "AllEntriesStoreFactory" - runTest(
-      store = factory.get(maxNumEntries = 3),
+      store = testModule.allEntriesStoreFactory.get(maxNumEntries = 3),
       updatesWithImpact = ListMap(
         // Add Transactions
         EntityModification.Add(createTransaction(day = 10, id = 10)) -> StateImpact.Change,
         EntityModification.Add(createTransaction(day = 9, id = 9)) -> StateImpact.Change,
         EntityModification.Add(createTransaction(day = 8, id = 8)) -> StateImpact.Change,
-        EntityModification.Add(createTransaction(day = 7, id = 7)) -> StateImpact.Undefined,
-        EntityModification.Add(createTransaction(day = 6, id = 6)) -> StateImpact.Undefined,
+        EntityModification.Add(createTransaction(day = 7, id = 7)) -> StateImpact.Change,
+        EntityModification.Add(createTransaction(day = 6, id = 6)) -> StateImpact.NoChange,
         // Remove Transactions
         EntityModification.Remove[Transaction](6) -> StateImpact.NoChange,
         EntityModification.Remove[Transaction](10) -> StateImpact.Change,
@@ -53,6 +56,32 @@ object StoreFactoryStateUpdateTest extends TestSuite {
         EntityModification.Remove[BalanceCheck](91) -> StateImpact.NoChange
       )
     )
+
+    "CashFlowEntriesStoreFactory" - runTest(
+      store =
+        testModule.cashFlowEntriesStoreFactory.get(moneyReservoir = testReservoirCardA, maxNumEntries = 2),
+      updatesWithImpact = ListMap(
+        // Add Transactions and BalanceChecks
+        EntityModification
+          .Add(createTransaction(day = 10, id = 10, reservoir = testReservoirCardA)) -> StateImpact.Change,
+        EntityModification
+          .Add(createTransaction(day = 9, id = 9, reservoir = testReservoirCardA)) -> StateImpact.Change,
+        EntityModification
+          .Add(createTransaction(day = 8, id = 8, reservoir = testReservoirCardA)) -> StateImpact.Change,
+        EntityModification
+          .Add(createTransaction(day = 7, id = 7, reservoir = testReservoirCardB)) -> StateImpact.Change,
+        EntityModification
+          .Add(createBalanceCheck(day = 6, id = 96, reservoir = testReservoirCardB)) -> StateImpact.Change,
+        EntityModification
+          .Add(createTransaction(day = 5, id = 5, reservoir = testReservoirCardB)) -> StateImpact.NoChange,
+        EntityModification
+          .Add(createBalanceCheck(day = 4, id = 94, reservoir = testReservoirCardB)) -> StateImpact.NoChange,
+        // Remove Transactions and BalanceChecks
+        EntityModification.Remove[Transaction](5) -> StateImpact.NoChange,
+        EntityModification.Remove[Transaction](6) -> StateImpact.Change,
+        EntityModification.Remove[Transaction](4) -> StateImpact.Change
+      )
+    )
   }
 
   private sealed trait StateImpact
@@ -60,5 +89,13 @@ object StoreFactoryStateUpdateTest extends TestSuite {
     object NoChange extends StateImpact
     object Change extends StateImpact
     object Undefined extends StateImpact
+  }
+
+  private final class ThisTestModule extends TestModule {
+
+    import com.softwaremill.macwire._
+
+    implicit val allEntriesStoreFactory: AllEntriesStoreFactory = wire[AllEntriesStoreFactory]
+    implicit val cashFlowEntriesStoreFactory: CashFlowEntriesStoreFactory = wire[CashFlowEntriesStoreFactory]
   }
 }
