23fd75c7d5bdb26af6ed5d004fa4e51f701c7cba
Jens Nyman <nymanjens.nj@gmail.com>
2017-11-18 14:11:43 +0000

    Move Main.html.scala to Layout

--- app/js/src/main/scala/flux/react/app/Layout.scala
+++ app/js/src/main/scala/flux/react/app/Layout.scala
@@ -1,20 +1,105 @@
 package flux.react.app
 
+import common.I18n
+import flux.react.ReactVdomUtils.^^
 import flux.react.router.RouterContext
 import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.PackageBase.VdomAttr
 import japgolly.scalajs.react.vdom.html_<^._
+import models.User
 
-final class Layout(implicit globalMessages: GlobalMessages, menu: Menu) {
+import scala.scalajs.js
+
+final class Layout(implicit globalMessages: GlobalMessages, menu: Menu, user: User, i18n: I18n) {
 
   private val component = ScalaComponent
     .builder[Props](getClass.getSimpleName)
-    .renderPC(
-      (_, props, children) =>
+    .renderPC((_, props, children) =>
+      <.div(
+        ^.id := "wrapper",
+        // Navigation
+        <.nav(
+          ^.className := "navbar navbar-default navbar-static-top",
+          ^.role := "navigation",
+          ^.style := js.Dictionary("margin-bottom" -> 0),
+          <.div(
+            ^.className := "navbar-header",
+            <.button(
+              ^.tpe := "button",
+              ^.className := "navbar-toggle",
+              VdomAttr("data-toggle") := "collapse",
+              VdomAttr("data-target") := ".navbar-collapse",
+              <.span(^.className := "sr-only", "Toggle navigation"),
+              <.span(^.className := "icon-bar"),
+              <.span(^.className := "icon-bar"),
+              <.span(^.className := "icon-bar")
+            ),
+            <.a(
+              ^.className := "navbar-brand",
+              ^.href := "@routes.Application.index",
+              "Family Accounting Tool")
+          ),
+          <.ul(
+            ^.className := "nav navbar-top-links navbar-right",
+            <.li(
+              ^.className := "dropdown",
+              <.a(
+                ^.className := "dropdown-toggle",
+                VdomAttr("data-toggle") := "dropdown",
+                ^.href := "#",
+                <.i(^.className := "fa fa-user fa-fw"),
+                " ",
+                <.i(^.className := "fa fa-caret-down")
+              ),
+              <.ul(
+                ^.className := "dropdown-menu dropdown-user",
+                <.li(
+                  <.a(
+                    ^.href := "@routes.Application.profile",
+                    <.i(^.className := "fa fa-user fa-fw"),
+                    " ",
+                    i18n("facto.user-profile"))),
+                ^^.ifThen(user.loginName == "admin") {
+                  <.li(
+                    <.a(
+                      ^.href := "@routes.Application.administration",
+                      <.i(^.className := "fa fa-cogs fa-fw"),
+                      " ",
+                      i18n("facto.user-administration")))
+                },
+                <.li(^.className := "divider"),
+                <.li(
+                  <.a(
+                    ^.href := "@routes.Auth.logout",
+                    " ",
+                    <.i(^.className := "fa fa-sign-out fa-fw"),
+                    i18n("facto.logout")))
+              )
+            )
+          )
+        ),
         <.div(
-          menu(props.router),
-          globalMessages(),
-          children
-      ))
+          ^.className := "navbar-default sidebar",
+          ^.role := "navigation",
+          <.div(^.className := "sidebar-nav navbar-collapse"),
+          menu(props.router)),
+        // Page Content
+        <.div(
+          ^.id := "page-wrapper",
+          <.div(
+            ^.className := "container-fluid",
+            <.div(
+              ^.className := "row",
+              <.div(
+                ^.className := "col-lg-12",
+                globalMessages(),
+                children,
+                <.hr(),
+                <.span(^.dangerouslySetInnerHtml := "&copy;"),
+                " 2016 Jens Nyman"))
+          )
+        )
+    ))
     .build
 
   // **************** API ****************//
