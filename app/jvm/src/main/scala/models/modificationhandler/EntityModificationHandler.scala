dc1014c17fc9968d30e0065d0142c4f974ad5ed1
Jens Nyman <nymanjens.nj@gmail.com>
2017-11-27 21:11:24 +0000

    Add EntityModificationHandler

---
+++ app/jvm/src/main/scala/models/modificationhandler/EntityModificationHandler.scala
@@ -0,0 +1,67 @@
+package models.modificationhandler
+
+import com.google.inject.Inject
+import common.time.Clock
+import models.SlickEntityAccess
+import models.manager.SlickEntityManager
+import models.modification.EntityType.{
+  BalanceCheckType,
+  ExchangeRateMeasurementType,
+  TransactionGroupType,
+  TransactionType,
+  UserType
+}
+import models.modification.{
+  EntityModification,
+  EntityModificationEntity,
+  EntityType,
+  SlickEntityModificationEntityManager
+}
+import models.user.User
+
+import scala.collection.immutable.Seq
+
+/**
+  * Handles storage and application of entity modifications.
+  *
+  * This class is the only point of entry for making changes to the database. Don't call the entity managers directly.
+  */
+final class EntityModificationHandler @Inject()(
+    implicit clock: Clock,
+    entityAccess: SlickEntityAccess,
+    entityModificationManager: SlickEntityModificationEntityManager) {
+
+  def persistEntityModifications(modifications: Seq[EntityModification])(implicit user: User): Unit = {
+    for (modification <- modifications) {
+      // Apply modification
+      val entityType = modification.entityType
+      modification match {
+        case EntityModification.Add(entity) =>
+          getManager(entityType).addWithId(entity.asInstanceOf[entityType.get])
+        case EntityModification.Update(entity) =>
+          getManager(entityType).update(entity.asInstanceOf[entityType.get])
+        case EntityModification.Remove(entityId) =>
+          getManager(entityType).delete(getManager(entityType).findById(entityId))
+      }
+
+      // Add modification
+      entityModificationManager.add(
+        EntityModificationEntity(
+          userId = user.id,
+          modification = modification,
+          date = clock.now
+        ))
+    }
+  }
+
+  private def getManager(entityType: EntityType.any): SlickEntityManager[entityType.get, _] = {
+    val manager = entityType match {
+      case UserType => entityAccess.userManager
+      case TransactionType => entityAccess.transactionManager
+      case TransactionGroupType => entityAccess.transactionGroupManager
+      case BalanceCheckType => entityAccess.balanceCheckManager
+      case ExchangeRateMeasurementType => entityAccess.exchangeRateMeasurementManager
+    }
+    manager.asInstanceOf[SlickEntityManager[entityType.get, _]]
+  }
+}
