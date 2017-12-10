2f321b2c64043b412e943c6766d54c1012afbb8c
Jens Nyman <nymanjens.nj@gmail.com>
2017-12-10 17:43:49 +0000

    Add ReactAutosuggest facade

---
+++ app/js/src/main/scala/jsfacades/ReactAutosuggest.scala
@@ -0,0 +1,72 @@
+package jsfacades
+
+import japgolly.scalajs.react.vdom.VdomElement
+import japgolly.scalajs.react.{Children, JsComponent}
+import org.scalajs.dom.ext.KeyCode
+
+import scala.collection.immutable.Seq
+import scala.scalajs.js
+import scala.scalajs.js.JSConverters._
+import scala2js.Converters._
+
+object ReactAutosuggest {
+
+  // **************** API ****************//
+  def apply(suggestions: Seq[String],
+            onSuggestionsFetchRequested: String => Unit,
+            onSuggestionsClearRequested: () => Unit,
+            renderSuggestion: String => VdomElement,
+            inputProps: InputProps,
+            theme: Theme) = {
+    val component = JsComponent[js.Object, Children.None, Null](js.Dynamic.global.Autosuggest)
+    component(
+      Props(
+        suggestions = suggestions.toJSArray,
+        onSuggestionsFetchRequested = onSuggestionsFetchRequested,
+        onSuggestionsClearRequested = onSuggestionsClearRequested,
+        getSuggestionValue = s => s,
+        renderSuggestion = renderSuggestion,
+        inputProps = js.Dynamic.literal(
+          value = inputProps.value,
+          onChange = (event: js.Any, params: js.Dynamic) => inputProps.onChange(params.newValue.toString)),
+        theme = js.Dynamic.literal(
+          container = theme.container,
+          input = theme.input,
+          suggestionsContainer = theme.suggestionsContainer,
+          suggestionsList = theme.suggestionsList,
+          suggestion = theme.suggestion,
+          suggestionHighlighted = theme.suggestionHighlighted
+        )
+      ).toJsObject)
+  }
+
+  // **************** Public inner types ****************//
+  case class InputProps(value: String, onChange: String => Unit)
+
+  case class Theme(container: String,
+                   input: String,
+                   suggestionsContainer: String,
+                   suggestionsList: String,
+                   suggestion: String,
+                   suggestionHighlighted: String)
+
+  // **************** Private inner types ****************//
+  private case class Props(suggestions: js.Array[String],
+                           onSuggestionsFetchRequested: js.Function1[String, Unit],
+                           onSuggestionsClearRequested: js.Function0[Unit],
+                           getSuggestionValue: js.Function1[String, String],
+                           renderSuggestion: js.Function1[String, VdomElement],
+                           inputProps: js.Object,
+                           theme: js.Object) {
+    def toJsObject: js.Object =
+      js.Dynamic.literal(
+        suggestions = suggestions,
+        onSuggestionsFetchRequested = onSuggestionsFetchRequested,
+        onSuggestionsClearRequested = onSuggestionsClearRequested,
+        getSuggestionValue = getSuggestionValue,
+        renderSuggestion = renderSuggestion,
+        inputProps = inputProps,
+        theme = theme
+      )
+  }
+}
