e7f6ea595edddb1ab7c23815528d527417c880ef
Jens Nyman <nymanjens.nj@gmail.com>
2017-12-17 20:51:01 +0000

    Add ApiBackedRemoteDatabaseProxy

--- app/js/src/main/scala/models/access/Module.scala
+++ app/js/src/main/scala/models/access/Module.scala
@@ -1,22 +1,26 @@
 package models.access
 
 import api.ScalaJsApiClient
+import common.time.Clock
 import models.user.User
 
 import scala.async.Async.{async, await}
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
-final class Module(user: User)(implicit scalaJsApiClient: ScalaJsApiClient) {
+final class Module(user: User)(implicit scalaJsApiClient: ScalaJsApiClient, clock: Clock) {
 
-  private lazy val localDatabase: Future[LocalDatabase] =
-    LocalDatabase.createFuture(encryptionSecret = user.databaseEncryptionKey)
-  implicit val remoteDatabaseProxy: Future[RemoteDatabaseProxy] = async {
-    val db = await(localDatabase)
-    val proxy = await(LocallyClonedRemoteDatabaseProxy.create(scalaJsApiClient, db))
+//  private lazy val localDatabase: Future[LocalDatabase] =
+//    LocalDatabase.createFuture(encryptionSecret = user.databaseEncryptionKey)
+//  implicit val remoteDatabaseProxy: Future[RemoteDatabaseProxy] = async {
+//    val db = await(localDatabase)
+//    val proxy = await(LocallyClonedRemoteDatabaseProxy.create(scalaJsApiClient, db))
+//
+//    proxy.startSchedulingModifiedEntityUpdates()
+//
+//    proxy
+//  }
 
-    proxy.startSchedulingModifiedEntityUpdates()
-
-    proxy
-  }
+  implicit val remoteDatabaseProxy: Future[RemoteDatabaseProxy] =
+    Future.successful(new ApiBackedRemoteDatabaseProxy)
 }
