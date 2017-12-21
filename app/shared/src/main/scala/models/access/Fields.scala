52ed9e9191d88114deb214f73b8eb87fe38713c2
Jens Nyman <nymanjens.nj@gmail.com>
2017-12-21 14:54:48 +0000

    Improved ModelField

--- app/shared/src/main/scala/models/access/Fields.scala
+++ app/shared/src/main/scala/models/access/Fields.scala
@@ -8,57 +8,63 @@ import models.user.User
 import scala.collection.immutable.Seq
 
 object Fields {
-  def id[E <: Entity]: ModelField[Long, E] = ModelField("id")
+  private def id[E <: Entity]: ModelField[Long, E] = new ModelField("id", _.idOption getOrElse -1)
 
   object User {
     private type E = User
 
-    val loginName: ModelField[String, E] = ModelField("loginName")
-    val passwordHash: ModelField[String, E] = ModelField("passwordHash")
-    val name: ModelField[String, E] = ModelField("name")
-    val databaseEncryptionKey: ModelField[String, E] = ModelField("databaseEncryptionKey")
-    val expandCashFlowTablesByDefault: ModelField[Boolean, E] = ModelField("expandCashFlowTablesByDefault")
+    val loginName: ModelField[String, E] = new ModelField("loginName", _.loginName)
+    val passwordHash: ModelField[String, E] = new ModelField("passwordHash", _.passwordHash)
+    val name: ModelField[String, E] = new ModelField("name", _.name)
+    val databaseEncryptionKey: ModelField[String, E] =
+      new ModelField("databaseEncryptionKey", _.databaseEncryptionKey)
+    val expandCashFlowTablesByDefault: ModelField[Boolean, E] =
+      new ModelField("expandCashFlowTablesByDefault", _.expandCashFlowTablesByDefault)
   }
 
   object Transaction {
     private type E = models.accounting.Transaction
 
-    val transactionGroupId: ModelField[Long, E] = ModelField("transactionGroupId")
-    val issuerId: ModelField[Long, E] = ModelField("issuerId")
-    val beneficiaryAccountCode: ModelField[String, E] = ModelField("beneficiaryAccountCode")
-    val moneyReservoirCode: ModelField[String, E] = ModelField("moneyReservoirCode")
-    val categoryCode: ModelField[String, E] = ModelField("categoryCode")
-    val description: ModelField[String, E] = ModelField("description")
-    val flowInCents: ModelField[Long, E] = ModelField("flowInCents")
-    val detailDescription: ModelField[String, E] = ModelField("detailDescription")
-    val tags: ModelField[Seq[String], E] = ModelField("tags")
-    val createdDate: ModelField[LocalDateTime, E] = ModelField("createdDate")
-    val transactionDate: ModelField[LocalDateTime, E] = ModelField("transactionDate")
-    val consumedDate: ModelField[LocalDateTime, E] = ModelField("consumedDate")
+    val transactionGroupId: ModelField[Long, E] = new ModelField("transactionGroupId", _.transactionGroupId)
+    val issuerId: ModelField[Long, E] = new ModelField("issuerId", _.issuerId)
+    val beneficiaryAccountCode: ModelField[String, E] =
+      new ModelField("beneficiaryAccountCode", _.beneficiaryAccountCode)
+    val moneyReservoirCode: ModelField[String, E] =
+      new ModelField("moneyReservoirCode", _.moneyReservoirCode)
+    val categoryCode: ModelField[String, E] = new ModelField("categoryCode", _.categoryCode)
+    val description: ModelField[String, E] = new ModelField("description", _.description)
+    val flowInCents: ModelField[Long, E] = new ModelField("flowInCents", _.flowInCents)
+    val detailDescription: ModelField[String, E] = new ModelField("detailDescription", _.detailDescription)
+    val tags: ModelField[Seq[String], E] = new ModelField("tags", _.tags)
+    val createdDate: ModelField[LocalDateTime, E] = new ModelField("createdDate", _.createdDate)
+    val transactionDate: ModelField[LocalDateTime, E] = new ModelField("transactionDate", _.transactionDate)
+    val consumedDate: ModelField[LocalDateTime, E] = new ModelField("consumedDate", _.consumedDate)
   }
 
   object TransactionGroup {
     private type E = models.accounting.TransactionGroup
 
-    val createdDate: ModelField[LocalDateTime, E] = ModelField("createdDate")
+    val createdDate: ModelField[LocalDateTime, E] = new ModelField("createdDate", _.createdDate)
   }
 
   object BalanceCheck {
     private type E = models.accounting.BalanceCheck
 
-    val issuerId: ModelField[Long, E] = ModelField("issuerId")
-    val moneyReservoirCode: ModelField[String, E] = ModelField("moneyReservoirCode")
-    val balanceInCents: ModelField[Long, E] = ModelField("balanceInCents")
-    val createdDate: ModelField[LocalDateTime, E] = ModelField("createdDate")
-    val checkDate: ModelField[LocalDateTime, E] = ModelField("checkDate")
+    val issuerId: ModelField[Long, E] = new ModelField("issuerId", _.issuerId)
+    val moneyReservoirCode: ModelField[String, E] =
+      new ModelField("moneyReservoirCode", _.moneyReservoirCode)
+    val balanceInCents: ModelField[Long, E] = new ModelField("balanceInCents", _.balanceInCents)
+    val createdDate: ModelField[LocalDateTime, E] = new ModelField("createdDate", _.createdDate)
+    val checkDate: ModelField[LocalDateTime, E] = new ModelField("checkDate", _.checkDate)
   }
 
   object ExchangeRateMeasurement {
     private type E = ExchangeRateMeasurement
 
-    val date: ModelField[LocalDateTime, E] = ModelField("date")
-    val foreignCurrencyCode: ModelField[String, E] = ModelField("foreignCurrencyCode")
-    val ratioReferenceToForeignCurrency: ModelField[Double, E] = ModelField(
-      "ratioReferenceToForeignCurrency")
+    val date: ModelField[LocalDateTime, E] = new ModelField("date", _.date)
+    val foreignCurrencyCode: ModelField[String, E] =
+      new ModelField("foreignCurrencyCode", _.foreignCurrencyCode)
+    val ratioReferenceToForeignCurrency: ModelField[Double, E] =
+      new ModelField("ratioReferenceToForeignCurrency", _.ratioReferenceToForeignCurrency)
   }
 }
--- app/shared/src/main/scala/models/access/ModelField.scala
+++ app/shared/src/main/scala/models/access/ModelField.scala
@@ -1,5 +1,9 @@
 package models.access
 
+import java.util.Objects
+
+import scala.reflect.ClassTag
+
 /**
   * Represents a field in an model entity.
   *
@@ -7,4 +11,18 @@ package models.access
   * @tparam V The type of the values
   * @tparam E The type corresponding to the entity that contains this field
   */
-case class ModelField[V, E](name: String)
+final class ModelField[V, E] private[access] (val name: String, accessor: E => V)(
+    implicit private val valueClassTag: ClassTag[V]) {
+
+  def valueClass: Class[_] = valueClassTag.runtimeClass
+  def get(entity: E): V = accessor(entity)
+
+  override def equals(any: scala.Any) = {
+    any match {
+      case that: ModelField[_, _] => this.name == that.name && this.valueClass == that.valueClass
+      case _ => false
+    }
+
+  }
+  override def hashCode() = Objects.hash(name, valueClass)
+}
