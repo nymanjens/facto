9920f6174e4a45d5e85592ec2a4dd6a913482eb9
Jens Nyman <nymanjens.nj@gmail.com>
2017-12-25 21:29:09 +0000

    DbQueryImplicits now supports any picklable ordering

--- app/shared/src/main/scala/models/access/DbQueryImplicits.scala
+++ app/shared/src/main/scala/models/access/DbQueryImplicits.scala
@@ -1,21 +1,21 @@
 package models.access
 
 import common.time.LocalDateTime
-import models.access.DbQuery.Filter
+import models.access.DbQuery.{Filter, PicklableOrdering}
 
 import scala.collection.immutable.Seq
 
 object DbQueryImplicits {
-  implicit class KeyWrapper[E, V](field: ModelField[V, E]) {
+  implicit class KeyWrapper[V, E](field: ModelField[V, E]) {
     def isEqualTo(value: V): Filter[E] = Filter.Equal(field, value)
     def isNotEqualTo(value: V): Filter[E] = Filter.NotEqual(field, value)
     def isAnyOf(values: Seq[V]): Filter[E] = Filter.AnyOf(field, values)
     def isNoneOf(values: Seq[V]): Filter[E] = Filter.NoneOf(field, values)
   }
-  implicit class OrderedKeyWrapper[E](field: ModelField[LocalDateTime, E]) {
-    def <(value: LocalDateTime): Filter[E] = Filter.LessThan(field, value)
-    def >(value: LocalDateTime): Filter[E] = Filter.GreaterThan(field, value)
-    def >=(value: LocalDateTime): Filter[E] = Filter.GreaterOrEqualThan(field, value)
+  implicit class OrderedKeyWrapper[V: PicklableOrdering, E](field: ModelField[V, E]) {
+    def <(value: V): Filter[E] = Filter.LessThan(field, value)
+    def >(value: V): Filter[E] = Filter.GreaterThan(field, value)
+    def >=(value: V): Filter[E] = Filter.GreaterOrEqualThan(field, value)
   }
 
   implicit class StringKeyWrapper[E](field: ModelField[String, E]) {
