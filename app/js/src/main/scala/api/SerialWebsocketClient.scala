489085939429c2452ebe5d73686d8f665112e3b6
Jens Nyman <nymanjens.nj@gmail.com>
2017-12-29 15:10:43 +0000

    Fix SerialWebsocketClient bugs

--- app/js/src/main/scala/api/SerialWebsocketClient.scala
+++ app/js/src/main/scala/api/SerialWebsocketClient.scala
@@ -1,10 +1,5 @@
 package api
 
-import scala.scalajs.js
-import scala.scalajs.js.JSConverters._
-import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
-import scala.async.Async.{async, await}
-import scala.scalajs.js.typedarray.{ArrayBuffer, _}
 import java.nio.ByteBuffer
 
 import api.Picklers._
@@ -12,32 +7,41 @@ import common.LoggingUtils.logExceptions
 import org.scalajs.dom
 import org.scalajs.dom.{CloseEvent, ErrorEvent, Event, MessageEvent, _}
 
+import scala.async.Async.{async, await}
+import scala.collection.mutable
 import scala.concurrent.{Future, Promise}
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.scalajs.js.typedarray.{ArrayBuffer, _}
 
 private[api] final class SerialWebsocketClient(websocketPath: String) {
   require(!websocketPath.startsWith("/"))
 
-  val websocket = initWebsocket()
-  val websocketReadyPromise: Promise[Unit] = Promise()
-  var responseMessagePromise: Promise[ByteBuffer] = Promise.successful(ByteBuffer.allocate(0))
+  var (websocket, websocketReadyPromise): (WebSocket, Promise[Unit]) = initWebsocket()
+  val responseMessagePromises: mutable.Buffer[Promise[ByteBuffer]] = mutable.Buffer()
 
   def sendAndReceive(request: ByteBuffer): Future[ByteBuffer] = async {
+    val lastMessagePromise: Option[Promise[_]] = responseMessagePromises.lastOption
+    val thisMessagePromise: Promise[ByteBuffer] = Promise()
+    responseMessagePromises += thisMessagePromise
+
     // Wait until websocket is initialized and previous request is done
     await(websocketReadyPromise.future)
-    await(responseMessagePromise.future)
-
-    logExceptions {
-      responseMessagePromise = Promise()
-
-      websocket.send(new Blob(toByteArray(request).toJSArray.asInstanceOf[js.Array[js.Any]]))
+    if (lastMessagePromise.isDefined) {
+      await(lastMessagePromise.get.future)
     }
 
-    await(responseMessagePromise.future)
+    logExceptions {
+      websocket.send(toArrayBuffer(request))
+    }
+
+    await(thisMessagePromise.future)
   }
 
-  private def initWebsocket(): WebSocket = {
+  private def initWebsocket(): (WebSocket, Promise[Unit]) = {
     val websocket = new dom.WebSocket(s"ws://${dom.window.location.host}/$websocketPath")
+    val websocketReadyPromise: Promise[Unit] = Promise()
 
+    websocket.binaryType = "arraybuffer";
     websocket.onmessage = (e: MessageEvent) => {
       val bytes = TypedArrayBuffer.wrap(e.data.asInstanceOf[ArrayBuffer])
       onMessageReceived(bytes)
@@ -46,27 +50,47 @@ private[api] final class SerialWebsocketClient(websocketPath: String) {
       websocketReadyPromise.success((): Unit)
       logLine("Opened")
     }
-    websocket.onerror = (e: ErrorEvent) => {
-      responseMessagePromise.tryFailure(new RuntimeException(s"Error has occured: ${e.message}"))
-      logLine(s"Error has occured: ${e.message}")
-    }
-    websocket.onclose = (e: CloseEvent) => logLine("Closed")
 
-    websocket
+    websocket.onerror = (e: ErrorEvent) => {
+      val errorMessage = s"Error has occured: ${e.message}"
+      websocketReadyPromise.tryFailure(new RuntimeException(errorMessage))
+      responseMessagePromises.headOption.map(_.tryFailure(new RuntimeException(errorMessage)))
+      logLine(errorMessage)
+    }
+    websocket.onclose = (e: CloseEvent) => {
+      val errorMessage = s"WebSocket was closed: ${e.reason}"
+      websocketReadyPromise.tryFailure(new RuntimeException(errorMessage))
+      responseMessagePromises.headOption.map(_.tryFailure(new RuntimeException(errorMessage)))
+      logLine(errorMessage)
+
+      val (newWebsocket, newWebsocketReadyPromise) = initWebsocket()
+      this.websocket = newWebsocket
+      this.websocketReadyPromise = newWebsocketReadyPromise
+    }
+
+    (websocket, websocketReadyPromise)
   }
 
   private def onMessageReceived(bytes: ByteBuffer): Unit = {
-    if (responseMessagePromise.isCompleted) {
-      logLine("Warning: Received message without request")
-    } else {
-      responseMessagePromise.success(bytes)
+    responseMessagePromises.headOption match {
+      case Some(promise) if promise.isCompleted =>
+        throw new AssertionError("First promise in responseMessagePromises is completed. This is a bug!")
+      case Some(promise) =>
+        responseMessagePromises.remove(0)
+        promise.success(bytes)
+      case None =>
+        logLine("Warning: Received message without request")
     }
   }
 
-  private def toByteArray(byteBuffer: ByteBuffer): Array[Byte] = {
-    val array = Array.ofDim[Byte](byteBuffer.remaining())
-    byteBuffer.get(array)
-    array
+  private def toArrayBuffer(byteBuffer: ByteBuffer): ArrayBuffer = {
+    val length = byteBuffer.remaining()
+    val arrayBuffer = new ArrayBuffer(length)
+    var arrayBufferView = new Int8Array(arrayBuffer)
+    for (i <- 0 until length) {
+      arrayBufferView.set(i, byteBuffer.get())
+    }
+    arrayBuffer
   }
 
   private def logLine(line: String): Unit = println(s"  [WebSocket] $line")
