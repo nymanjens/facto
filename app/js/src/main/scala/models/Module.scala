ee9d5d7fa5758965b88a9f2aedd448d34c6e1ca7
Jens Nyman <nymanjens.nj@gmail.com>
2018-01-01 14:56:45 +0000

    Remove last js Managers

--- app/js/src/main/scala/models/Module.scala
+++ app/js/src/main/scala/models/Module.scala
@@ -3,14 +3,12 @@ package models
 import api.ScalaJsApi.GetInitialDataResponse
 import common.money.ExchangeRateManager
 import models.access.RemoteDatabaseProxy
-import models.money.{JsExchangeRateManager, JsExchangeRateMeasurementManager}
+import models.money.JsExchangeRateManager
 
 final class Module(implicit remoteDatabaseProxy: RemoteDatabaseProxy,
                    getInitialDataResponse: GetInitialDataResponse) {
 
   import com.softwaremill.macwire._
-
-  implicit lazy val exchangeRateMeasurementManager = wire[JsExchangeRateMeasurementManager]
 
   implicit lazy val entityAccess: JsEntityAccess = wire[JsEntityAccess]
   implicit lazy val exchangeRateManager: ExchangeRateManager =
