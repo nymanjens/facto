8575fb460dd61fde757580d64c82c77995b062cc
Jens Nyman <nymanjens.nj@gmail.com>
2018-01-16 20:35:04 +0000

    Fix JsExchangeRateManagerTest

--- app/js/src/test/scala/models/money/JsExchangeRateManagerTest.scala
+++ app/js/src/test/scala/models/money/JsExchangeRateManagerTest.scala
@@ -3,25 +3,30 @@ package models.money
 import java.time.Duration
 
 import common.money.Currency.{Eur, Gbp, Usd}
-import common.testing.TestModule
-import common.time.LocalDateTime
+import common.testing.{FakeJsEntityAccess, TestModule}
+import common.time.{Clock, LocalDateTime}
+import models.access.{EntityAccess, JsEntityAccess}
 import utest.{TestSuite, _}
 
+import scala.collection.SortedMap
 import scala2js.Converters._
 
 object JsExchangeRateManagerTest extends TestSuite {
 
   override def tests = TestSuite {
     val testModule = new TestModule()
-    val clock = testModule.fakeClock
-    val entityAccess = testModule.fakeEntityAccess
-    val exchangeRateManager: JsExchangeRateManager = testModule.exchangeRateManager
+    implicit val clock = testModule.fakeClock
+    implicit val entityAccess = testModule.fakeEntityAccess
+    val exchangeRateManager: JsExchangeRateManager =
+      new JsExchangeRateManager(
+        ratioReferenceToForeignCurrency = Map(
+          Gbp -> SortedMap(yesterdayPlusMillis(1000) -> 2.0),
+          Gbp -> SortedMap(yesterdayPlusMillis(2000) -> 3.0),
+          Gbp -> SortedMap(yesterdayPlusMillis(3000) -> 0.5),
+        )
+      )
 
     "getRatioSecondToFirstCurrency()" - {
-      persistGbpMeasurement(date = yesterdayPlusMillis(1000), ratio = 2)
-      persistGbpMeasurement(date = yesterdayPlusMillis(2000), ratio = 3)
-      persistGbpMeasurement(date = yesterdayPlusMillis(3000), ratio = 0.5)
-
       exchangeRateManager.getRatioSecondToFirstCurrency(Eur, Eur, clock.now) ==> 1.0
       exchangeRateManager.getRatioSecondToFirstCurrency(Eur, Usd, clock.now) ==> 1.0
 
@@ -37,21 +42,20 @@ object JsExchangeRateManagerTest extends TestSuite {
       exchangeRateManager.getRatioSecondToFirstCurrency(Gbp, Eur, clock.now) ==> 0.5
 
       exchangeRateManager.getRatioSecondToFirstCurrency(Eur, Gbp, clock.now) ==> 2.0
-
-      persistGbpMeasurement(date = yesterdayPlusMillis(4000), ratio = 9)
-      exchangeRateManager.getRatioSecondToFirstCurrency(Gbp, Eur, clock.now) ==> 9.0
     }
 
-    def persistGbpMeasurement(date: LocalDateTime, ratio: Double): Unit = {
-      entityAccess.addWithRandomId(
-        ExchangeRateMeasurement(
-          date = date,
-          foreignCurrencyCode = Gbp.code,
-          ratioReferenceToForeignCurrency = ratio))
-    }
+  }
 
-    def yesterdayPlusMillis(millis: Long): LocalDateTime = {
-      clock.now.plus(Duration.ofDays(-1).plusMillis(millis))
-    }
+  def persistGbpMeasurement(date: LocalDateTime, ratio: Double)(
+      implicit entityAccess: FakeJsEntityAccess): Unit = {
+    entityAccess.addWithRandomId(
+      ExchangeRateMeasurement(
+        date = date,
+        foreignCurrencyCode = Gbp.code,
+        ratioReferenceToForeignCurrency = ratio))
+  }
+
+  def yesterdayPlusMillis(millis: Long)(implicit clock: Clock): LocalDateTime = {
+    clock.now.plus(Duration.ofDays(-1).plusMillis(millis))
   }
 }
