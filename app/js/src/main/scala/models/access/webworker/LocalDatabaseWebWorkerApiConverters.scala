4ecb31ffd25a7325a80d7820e4e86fcfbf05e11d
Jens Nyman <nymanjens.nj@gmail.com>
2018-03-23 19:48:08 +0000

    Implement LocalDatabaseWebWorkerApiConverters

--- app/js/src/main/scala/models/access/webworker/LocalDatabaseWebWorkerApiConverters.scala
+++ app/js/src/main/scala/models/access/webworker/LocalDatabaseWebWorkerApiConverters.scala
@@ -1,20 +1,42 @@
 package models.access.webworker
 
-import models.Entity
-import models.access.DbQuery
 import models.access.webworker.LocalDatabaseWebWorkerApi.WriteOperation
+import models.access.webworker.LocalDatabaseWebWorkerApi.WriteOperation._
 
 import scala.collection.immutable.Seq
-import scala.concurrent.Future
 import scala.scalajs.js
 import scala2js.Scala2Js
 
 object LocalDatabaseWebWorkerApiConverters {
 
   implicit object WriteOperationConverter extends Scala2Js.Converter[WriteOperation] {
+    private val insertNumber = 1
+    private val findAndRemoveNumber = 2
+    private val clearNumber = 3
+    private val saveDatabaseNumber = 4
+
     override def toJs(operation: WriteOperation) = {
-      ???
+      operation match {
+        case Insert(collectionName, obj) => js.Array[js.Any](insertNumber, collectionName, obj)
+        case FindAndRemove(collectionName, fieldName, fieldValue) =>
+          js.Array[js.Any](findAndRemoveNumber, collectionName, fieldName, fieldValue)
+        case Clear(collectionName) => js.Array[js.Any](clearNumber, collectionName)
+        case SaveDatabase          => js.Array[js.Any](saveDatabaseNumber)
+      }
     }
-    override def toScala(value: js.Any) = ???
+
+    override def toScala(value: js.Any) = {
+      val seq = value.asInstanceOf[js.Array[js.Any]].toVector
+      val firstElement = seq(0).asInstanceOf[Int]
+
+      (firstElement, seq) match {
+        case (`insertNumber`, Seq(_, collectionName, obj)) =>
+          Insert(collectionName.asInstanceOf[String], obj.asInstanceOf[js.Dictionary[js.Any]])
+        case (`findAndRemoveNumber`, Seq(_, collectionName, fieldName, fieldValue)) =>
+          FindAndRemove(collectionName.asInstanceOf[String], fieldName.asInstanceOf[String], fieldValue)
+        case (`clearNumber`, Seq(_, collectionName)) => Clear(collectionName.asInstanceOf[String])
+        case (`saveDatabaseNumber`, Seq(_))          => SaveDatabase
+      }
+    }
   }
 }
