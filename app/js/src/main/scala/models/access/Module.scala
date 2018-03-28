f0e8a35c694583ce8cec14dca7d26291ba7a4723
Jens Nyman <nymanjens.nj@gmail.com>
2018-03-28 21:55:01 +0100

    Install hybrid RemoteDatabaseProxy

--- app/js/src/main/scala/models/access/Module.scala
+++ app/js/src/main/scala/models/access/Module.scala
@@ -7,31 +7,30 @@ import models.user.User
 
 import scala.concurrent.Future
 
-final class Module(user: User)(implicit scalaJsApiClient: ScalaJsApiClient,
-                               clock: Clock,
-                               getInitialDataResponse: GetInitialDataResponse) {
-
-  private val webworkerModule = new models.access.webworker.Module()
-  implicit private val localDatabaseWebWorkerApiStub = webworkerModule.localDatabaseWebWorkerApiStub
+final class Module(implicit user: User,
+                   scalaJsApiClient: ScalaJsApiClient,
+                   clock: Clock,
+                   getInitialDataResponse: GetInitialDataResponse) {
 
   // Use LocallyClonedJsEntityAccess
-//  implicit val entityAccess: Future[JsEntityAccess] = async {
-//    val db = await(LocalDatabase.createFuture(encryptionSecret = user.databaseEncryptionKey))
-//    val entityAccess =
-//      await(LocallyClonedJsEntityAccess.create(scalaJsApiClient, db, getInitialDataResponse.allUsers))
+  implicit val entityAccess: JsEntityAccess = {
+    val webWorkerModule = new models.access.webworker.Module()
+    implicit val localDatabaseWebWorkerApiStub = webWorkerModule.localDatabaseWebWorkerApiStub
+    implicit val remoteDatabaseProxy = HybridRemoteDatabaseProxy.create()
+    val entityAccess = new JsEntityAccessImpl(getInitialDataResponse.allUsers)
+
+    entityAccess.startSchedulingModifiedEntityUpdates()
+
+    entityAccess
+  }
+
+  // Use ApiBackedJsEntityAccess
+//  implicit val entityAccess: JsEntityAccess = {
+//    implicit val remoteDatabaseProxy = new ApiBackedRemoteDatabaseProxy()
+//    val entityAccess = new JsEntityAccessImpl(getInitialDataResponse.allUsers)
 //
 //    entityAccess.startSchedulingModifiedEntityUpdates()
 //
 //    entityAccess
 //  }
-
-  // Use ApiBackedJsEntityAccess
-  implicit val entityAccess: Future[JsEntityAccess] = {
-    implicit val remoteDatabaseProxy = new ApiBackedRemoteDatabaseProxy()
-    val entityAccess = new JsEntityAccessImpl(getInitialDataResponse.allUsers)
-
-    entityAccess.startSchedulingModifiedEntityUpdates()
-
-    Future.successful(entityAccess)
-  }
 }
