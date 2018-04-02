0377a3c73c68f5b918df026640480d305c85ba8a
Jens Nyman <nymanjens.nj@gmail.com>
2018-04-02 21:12:22 +0100

    Add LocalDatabaseWebWorkerApiConvertersTest

---
+++ app/js/src/test/scala/models/access/webworker/LocalDatabaseWebWorkerApiConvertersTest.scala
@@ -0,0 +1,40 @@
+package models.access.webworker
+
+import models.access.webworker.LocalDatabaseWebWorkerApi.{LokiQuery, WriteOperation}
+import models.access.webworker.LocalDatabaseWebWorkerApiConverters._
+import utest._
+
+import scala.language.reflectiveCalls
+import scala.scalajs.js
+import scala2js.Scala2Js
+
+object LocalDatabaseWebWorkerApiConvertersTest extends TestSuite {
+
+  private val testObj: js.Dictionary[js.Any] = js.Dictionary("a" -> 1, "b" -> "2")
+
+  override def tests = TestSuite {
+    "WriteOperationConverter" - {
+      "Insert" - { testForwardAndBackward[WriteOperation](WriteOperation.Insert("test", testObj)) }
+      "Update" - { testForwardAndBackward[WriteOperation](WriteOperation.Update("test", testObj)) }
+      "Remove" - { testForwardAndBackward[WriteOperation](WriteOperation.Remove("test", "192837")) }
+      "Clear" - { testForwardAndBackward[WriteOperation](WriteOperation.Clear("test")) }
+      "SaveDatabase" - { testForwardAndBackward[WriteOperation](WriteOperation.SaveDatabase) }
+    }
+
+    "LokiQueryConverter" - {
+      testForwardAndBackward(LokiQuery(collectionName = "test"))
+      testForwardAndBackward(
+        LokiQuery(
+          collectionName = "test",
+          filter = Some(testObj),
+          sorting = Some(js.Array(js.Array[js.Any]("xx", 12))),
+          limit = Some(1238)))
+    }
+  }
+
+  private def testForwardAndBackward[T: Scala2Js.Converter](value: T): Unit = {
+    val jsValue = Scala2Js.toJs(value)
+    val newValue = Scala2Js.toScala[T](jsValue)
+    newValue ==> value
+  }
+}
