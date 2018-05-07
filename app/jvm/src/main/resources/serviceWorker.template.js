062c3f352b362e16d2a77152f64934b71e981e4d
Jens Nyman <nymanjens.nj@gmail.com>
2018-05-07 21:59:20 +0100

    cache getInitialData

--- app/jvm/src/main/resources/serviceWorker.template.js
+++ app/jvm/src/main/resources/serviceWorker.template.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'facto-v2';
-const ROOT_URL = new Request('/').url;
+const ROOT_URL = new Request('/').url.slice(0, -1);
 const APP_PAGE_PATH = '/appwithoutcreds/';
+const GET_INITIAL_DATA_PATH = '/scalajsapi/getInitialData';
 const SCRIPT_PATHS_TO_CACHE = [
   %SCRIPT_PATHS_TO_CACHE%
 ];
@@ -11,6 +12,8 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME)
       .then((cache) => {
         cache.add(APP_PAGE_PATH);
+        cache.add(
+            new Request(GET_INITIAL_DATA_PATH, {credentials: 'same-origin'}));
         cache.addAll(SCRIPT_PATHS_TO_CACHE);
       })
   )
@@ -18,12 +21,24 @@ self.addEventListener('install', (event) => {
 
 
 self.addEventListener('fetch', (event) => {
-  if(event.request.url.startsWith(ROOT_URL + 'app/')) {
-    console.log('  Fetching app at', event.request.url)
-    // Check whether we are still logged in
-    // If we are offline, return the cached app page.
+  if(event.request.url.startsWith(ROOT_URL + '/app/')) {
+    // Check whether we are still logged in. If we are offline, return the
+    // cached app page.
+    console.log('  Fetch or cache:', event.request.url)
     event.respondWith(
       fetch(event.request).catch(() => caches.match(APP_PAGE_PATH))
+    );
+  } else if(event.request.url == ROOT_URL + GET_INITIAL_DATA_PATH) {
+    // Initial data may change, e.g. when logging in. If we are oflfine, return
+    // the cached values.
+    console.log('  (Fetch and cache) or cache:', event.request.url)
+    event.respondWith(
+      fetch(event.request)
+          .then(response =>
+            cache
+                .put(event.request, response.clone())
+                .then(() => reponse))
+          .catch(() => caches.match(APP_PAGE_PATH))
     );
   } else {
     event.respondWith(
