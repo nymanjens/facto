3bc6a864a4e4cd6b11efd530bba08ee1678928e1
Jens Nyman <nymanjens.nj@gmail.com>
2018-05-07 15:48:22 +0100

    Document ScalaJsApiRequest

--- app/shared/src/main/scala/api/ScalaJsApiRequest.scala
+++ app/shared/src/main/scala/api/ScalaJsApiRequest.scala
@@ -4,4 +4,8 @@ import java.nio.ByteBuffer
 
 import api.Picklers._
 
+/**
+  * Picklable combination of method name (= path) and method arguments, representing a single call
+  * to a ScalaJS API method.
+  */
 case class ScalaJsApiRequest(path: String, args: Map[String, ByteBuffer])
