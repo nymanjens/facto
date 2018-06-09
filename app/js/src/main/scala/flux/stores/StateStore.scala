6dd19a8b75749dee3a072e5eac184f51dc3c5ca2
Jens Nyman <nymanjens.nj@gmail.com>
2018-06-09 14:44:26 +0200

    Add StateStore

---
+++ app/js/src/main/scala/flux/stores/StateStore.scala
@@ -0,0 +1,44 @@
+package flux.stores
+
+import scala.collection.immutable.Seq
+
+abstract class StateStore[State] {
+
+  private var _stateUpdateListeners: Seq[StateStore.Listener] = Seq()
+  private var isCallingListeners: Boolean = false
+
+  // **************** Public API ****************//
+  def state: State
+
+  final def register(listener: StateStore.Listener): Unit = {
+    checkNotCallingListeners()
+
+    _stateUpdateListeners = _stateUpdateListeners :+ listener
+  }
+
+  final def deregister(listener: StateStore.Listener): Unit = {
+    checkNotCallingListeners()
+
+    _stateUpdateListeners = _stateUpdateListeners.filter(_ != listener)
+  }
+
+  // **************** Implementation helper methods ****************//
+  protected final def invokeListeners(): Unit = {
+    checkNotCallingListeners()
+    isCallingListeners = true
+    _stateUpdateListeners.foreach(_.onStateUpdate())
+    isCallingListeners = false
+  }
+
+  protected final def stateUpdateListeners: Seq[StateStore.Listener] = _stateUpdateListeners
+
+  protected final def checkNotCallingListeners(): Unit = {
+    require(!isCallingListeners, "checkNotCallingListeners(): But isCallingListeners is true")
+  }
+}
+
+object StateStore {
+  trait Listener {
+    def onStateUpdate(): Unit
+  }
+}
