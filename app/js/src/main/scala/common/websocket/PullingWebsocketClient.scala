46010bffa8681583f1259f60569cede7768e58cc
Jens Nyman <nymanjens.nj@gmail.com>
2018-06-19 14:59:33 +0200

    Add PullingWebsocketClient

---
+++ app/js/src/main/scala/common/websocket/PullingWebsocketClient.scala
@@ -0,0 +1,44 @@
+package common.websocket
+
+import java.nio.ByteBuffer
+
+import org.scalajs.dom
+import org.scalajs.dom.{CloseEvent, ErrorEvent, Event, MessageEvent, _}
+
+import scala.scalajs.js.typedarray.{ArrayBuffer, _}
+
+final class PullingWebsocketClient(websocketPath: String,
+                                   onMessageReceived: ByteBuffer => Unit,
+                                   onSuccessfulOpen: () => Unit) {
+  require(!websocketPath.startsWith("/"))
+
+  openWebsocket()
+
+  private def openWebsocket(): Unit = {
+    val protocol = if (dom.window.location.protocol == "https:") "wss:" else "ws:"
+    val websocket = new dom.WebSocket(s"${protocol}//${dom.window.location.host}/$websocketPath")
+
+    websocket.binaryType = "arraybuffer";
+    websocket.onmessage = (e: MessageEvent) => {
+      val bytes = TypedArrayBuffer.wrap(e.data.asInstanceOf[ArrayBuffer])
+      onMessageReceived(bytes)
+    }
+    websocket.onopen = (e: Event) => {
+      logLine("Opened")
+      onSuccessfulOpen()
+    }
+    websocket.onerror = (e: ErrorEvent) => {
+      // Note: the given event turns out to be of type "error", but has an undefined message. This causes
+      // ClassCastException when accessing it as a String
+      val errorMessage = s"Error when connecting to WebSocket"
+      logLine(errorMessage)
+    }
+    websocket.onclose = (e: CloseEvent) => {
+      val errorMessage = s"WebSocket was closed: ${e.reason}"
+      logLine(errorMessage)
+      // TODO: Retry
+    }
+  }
+
+  private def logLine(line: String): Unit = console.log(s"  [PullingWebsocketClient] $line")
+}
