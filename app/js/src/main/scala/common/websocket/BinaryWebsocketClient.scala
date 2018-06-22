d869507600d6274fc1e30215575f409ceabfe626
Jens Nyman <nymanjens.nj@gmail.com>
2018-06-22 23:08:18 +0200

    Add BinaryWebsocketClient

---
+++ app/js/src/main/scala/common/websocket/BinaryWebsocketClient.scala
@@ -0,0 +1,78 @@
+package common.websocket
+
+import java.nio.ByteBuffer
+
+import common.LoggingUtils.logExceptions
+import org.scalajs.dom
+import org.scalajs.dom.{CloseEvent, ErrorEvent, Event, MessageEvent, _}
+
+import scala.async.Async.{async, await}
+import scala.concurrent.{Future, Promise}
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.scalajs.js.typedarray.{ArrayBuffer, _}
+
+final class BinaryWebsocketClient(name: String, jsWebsocket: WebSocket) {
+
+  def send(message: ByteBuffer): Unit = logExceptions {
+    jsWebsocket.send(toArrayBuffer(message))
+  }
+
+  def close(): Unit = async {
+    BinaryWebsocketClient.logLine(name, "Closing WebSocket...")
+    jsWebsocket.onclose = (e: CloseEvent) => {}
+    jsWebsocket.close()
+  }
+
+  private def toArrayBuffer(byteBuffer: ByteBuffer): ArrayBuffer = {
+    val length = byteBuffer.remaining()
+    val arrayBuffer = new ArrayBuffer(length)
+    var arrayBufferView = new Int8Array(arrayBuffer)
+    for (i <- 0 until length) {
+      arrayBufferView.set(i, byteBuffer.get())
+    }
+    arrayBuffer
+  }
+}
+object BinaryWebsocketClient {
+  def open(name: String,
+           websocketPath: String,
+           onMessageReceived: ByteBuffer => Unit,
+           onClose: () => Unit = () => {}): Future[BinaryWebsocketClient] = {
+    require(!websocketPath.startsWith("/"))
+
+    val protocol = if (dom.window.location.protocol == "https:") "wss:" else "ws:"
+    val jsWebsocket = new dom.WebSocket(s"${protocol}//${dom.window.location.host}/$websocketPath")
+    val jsWebsocketPromise: Promise[WebSocket] = Promise()
+
+    jsWebsocket.binaryType = "arraybuffer"
+    jsWebsocket.onmessage = (e: MessageEvent) =>
+      logExceptions {
+        val bytes = TypedArrayBuffer.wrap(e.data.asInstanceOf[ArrayBuffer])
+        onMessageReceived(bytes)
+    }
+    jsWebsocket.onopen = (e: Event) =>
+      logExceptions {
+        jsWebsocketPromise.success(jsWebsocket)
+        logLine(name, "Opened")
+    }
+    jsWebsocket.onerror = (e: ErrorEvent) =>
+      logExceptions {
+        // Note: the given event turns out to be of type "error", but has an undefined message. This causes
+        // ClassCastException when accessing it as a String
+        val errorMessage = s"Error when connecting to WebSocket"
+        jsWebsocketPromise.tryFailure(new RuntimeException(errorMessage))
+        logLine(name, errorMessage)
+    }
+    jsWebsocket.onclose = (e: CloseEvent) =>
+      logExceptions {
+        val errorMessage = s"WebSocket was closed: ${e.reason}"
+        jsWebsocketPromise.tryFailure(new RuntimeException(errorMessage))
+        logLine(name, errorMessage)
+        onClose()
+    }
+
+    jsWebsocketPromise.future
+  }
+
+  private def logLine(name: String, line: String): Unit = console.log(s"  [$name] $line")
+}
