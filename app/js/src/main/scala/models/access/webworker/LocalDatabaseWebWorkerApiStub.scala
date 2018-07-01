23308a52081d39fa34c3b5dedfee02a2094dd4f5
Jens Nyman <nymanjens.nj@gmail.com>
2018-07-01 16:47:39 +0200

    Increase database create timeout

--- app/js/src/main/scala/models/access/webworker/LocalDatabaseWebWorkerApiStub.scala
+++ app/js/src/main/scala/models/access/webworker/LocalDatabaseWebWorkerApiStub.scala
@@ -25,7 +25,7 @@ private[webworker] final class LocalDatabaseWebWorkerApiStub extends LocalDataba
   private val worker: Worker = initializeWebWorker()
 
   override def create(dbName: String, inMemory: Boolean) = {
-    sendAndReceive(MethodNumbers.create, Seq(dbName, inMemory)).map(_ => (): Unit)
+    sendAndReceive(MethodNumbers.create, Seq(dbName, inMemory), timeout = 10.seconds).map(_ => (): Unit)
   }
 
   override def executeDataQuery(lokiQuery: LocalDatabaseWebWorkerApi.LokiQuery) =
