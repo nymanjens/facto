19a8bac21be0a7e27ded6f5f898dee475bca2bb7
Jens Nyman <nymanjens.nj@gmail.com>
2018-07-03 15:41:26 +0200

    Add ListenableTest

---
+++ app/js/src/test/scala/common/ListenableTest.scala
@@ -0,0 +1,44 @@
+package common
+
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.async.Async.{async, await}
+import common.Listenable.WritableListenable
+import common.testing.Awaiter
+import utest.{TestSuite, _}
+
+import scala.concurrent.{Await, Promise}
+import scala2js.Converters._
+
+object ListenableTest extends TestSuite {
+
+  override def tests = TestSuite {
+
+    "WritableListenable" - {
+      val listenable: WritableListenable[Int] = WritableListenable(123)
+
+      "get" - {
+        listenable.get ==> 123
+      }
+      "set" - {
+        listenable.set(456)
+        listenable.get ==> 456
+      }
+      "calls listener when value changes" - async {
+        val promise = Promise[Int]()
+        listenable.registerListener(newValue => promise.success(newValue))
+
+        listenable.set(456)
+
+        await(Awaiter.expectComplete(promise.future, 456))
+      }
+      "doesn't call listener when value stays the same" - async {
+        val promise = Promise[Int]()
+        listenable.registerListener(newValue => promise.success(newValue))
+
+        listenable.set(123)
+
+        await(Awaiter.expectNeverComplete(promise.future))
+      }
+    }
+  }
+}
