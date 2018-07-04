7bd1653c34ef168021145d880f6070d7314798e4
Jens Nyman <nymanjens.nj@gmail.com>
2018-07-04 23:16:36 +0200

    Add password update form without functionality

--- app/js/src/main/scala/flux/react/app/usermanagement/UserProfile.scala
+++ app/js/src/main/scala/flux/react/app/usermanagement/UserProfile.scala
@@ -1,11 +1,12 @@
 package flux.react.app.usermanagement
 
 import common.I18n
-import common.LoggingUtils.logExceptions
+import common.LoggingUtils.{LogExceptionsCallback, logExceptions}
 import flux.react.ReactVdomUtils.<<
 import flux.react.router.{Page, RouterContext}
 import flux.react.uielements
 import flux.react.uielements.Panel
+import flux.react.uielements.input.bootstrap
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import models.access.EntityAccess
@@ -18,11 +19,8 @@ private[app] final class UserProfile(implicit user: User, i18n: I18n) {
 
   private val component = ScalaComponent
     .builder[Props](getClass.getSimpleName)
-    .renderP(($, props) =>
-      logExceptions {
-        implicit val router = props.router
-        <.span("Hello world")
-    })
+    .initialState(State())
+    .renderBackend[Backend]
     .build
 
   // **************** API ****************//
@@ -32,4 +30,103 @@ private[app] final class UserProfile(implicit user: User, i18n: I18n) {
 
   // **************** Private inner types ****************//
   private case class Props(router: RouterContext)
+  private case class State(showErrorMessages: Boolean = false, globalErrors: Seq[String] = Seq())
+
+  private final class Backend(val $ : BackendScope[Props, State]) {
+
+    private val oldPasswordRef = bootstrap.TextInput.ref()
+    private val passwordRef = bootstrap.TextInput.ref()
+    private val passwordVerificationRef = bootstrap.TextInput.ref()
+
+    def render(props: Props, state: State) = logExceptions {
+      implicit val router = props.router
+      <.span(
+        uielements.PageHeader(router.currentPage),
+        <.div(
+          ^.className := "row",
+          <.form(
+            ^.className := "form-horizontal",
+            uielements.HalfPanel(title = <.span(i18n("facto.change-password")))(
+              {
+                for (error <- state.globalErrors) yield {
+                  <.div(^.className := "alert alert-danger", ^.key := error, error)
+                }
+              }.toVdomArray,
+              bootstrap.TextInput(
+                ref = bootstrap.TextInput.ref(),
+                name = "loginName",
+                label = i18n("facto.login-name"),
+                defaultValue = user.loginName,
+                disabled = true
+              ),
+              bootstrap.TextInput(
+                ref = oldPasswordRef,
+                name = "oldPassword",
+                label = i18n("facto.old-password"),
+                inputType = "password",
+                required = true,
+                showErrorMessage = state.showErrorMessages,
+                focusOnMount = true,
+              ),
+              bootstrap.TextInput(
+                ref = passwordRef,
+                name = "password",
+                label = i18n("facto.password"),
+                inputType = "password",
+                required = true,
+                showErrorMessage = state.showErrorMessages
+              ),
+              bootstrap.TextInput(
+                ref = passwordVerificationRef,
+                name = "passwordVerification",
+                label = i18n("facto.retype-password"),
+                inputType = "password",
+                required = true,
+                showErrorMessage = state.showErrorMessages
+              ),
+              <.button(
+                ^.tpe := "submit",
+                ^.className := "btn btn-default",
+                ^.onClick ==> onSubmit,
+                i18n("facto.ok"))
+            )
+          )
+        )
+      )
+    }
+
+    private def onSubmit(e: ReactEventFromInput): Callback = LogExceptionsCallback {
+      val props = $.props.runNow()
+      e.preventDefault()
+
+      $.modState(state =>
+        logExceptions {
+          var newState = State(showErrorMessages = true)
+
+          val maybeNewPassword = for {
+            oldPassword <- oldPasswordRef().value
+            password <- passwordRef().value
+            passwordVerification <- passwordVerificationRef().value
+            validPassword <- {
+              // TODO: Do verification
+              Some(password)
+            }
+          } yield validPassword
+
+          maybeNewPassword match {
+            case Some(newPassword) =>
+              // TODO: Submit
+
+              // Clear form
+              oldPasswordRef().setValue("")
+              passwordRef().setValue("")
+              passwordVerificationRef().setValue("")
+              newState = State()
+
+            case None =>
+          }
+          newState
+      }).runNow()
+    }
+  }
 }
