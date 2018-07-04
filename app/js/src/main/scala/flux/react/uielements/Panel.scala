ffb678601ea965baafa570a2653641c8631cb8e9
Jens Nyman <nymanjens.nj@gmail.com>
2018-07-04 23:07:36 +0200

    Allow caller to define panel width

--- app/js/src/main/scala/flux/react/uielements/Panel.scala
+++ app/js/src/main/scala/flux/react/uielements/Panel.scala
@@ -7,14 +7,14 @@ import japgolly.scalajs.react.vdom.html_<^._
 import scala.collection.immutable.Seq
 
 object Panel {
-  private case class Props(title: String, panelClasses: Seq[String])
+  private case class Props(title: String, panelClasses: Seq[String], widthInColumns: Int)
   private val component = ScalaComponent
     .builder[Props](getClass.getSimpleName)
     .renderPC((_, props, children) =>
       <.div(
         ^^.classes("row" +: props.panelClasses),
         <.div(
-          ^^.classes("col-lg-12"),
+          ^^.classes(s"col-lg-${props.widthInColumns}"),
           <.div(
             ^^.classes("panel panel-default"),
             <.div(^^.classes("panel-heading"), props.title),
@@ -23,12 +23,13 @@ object Panel {
     ))
     .build
 
-  def apply(title: String, panelClasses: Seq[String] = Seq(), key: String = null)(
+  def apply(title: String, panelClasses: Seq[String] = Seq(), key: String = null, widthInColumns: Int = 12)(
       children: VdomNode*): VdomElement = {
+    val props = Props(title, panelClasses, widthInColumns)
     if (key == null) {
-      component(Props(title, panelClasses))(children: _*)
+      component(props)(children: _*)
     } else {
-      component.withKey(key).apply(Props(title, panelClasses))(children: _*)
+      component.withKey(key).apply(props)(children: _*)
     }
   }
 }
