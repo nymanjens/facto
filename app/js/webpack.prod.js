765fb5737466cf6f606ecdce42748d45c364786e
Jens Nyman <nymanjens.nj@gmail.com>
2018-07-17 18:09:42 +0200

    Add NODE_ENV = production to prod webpack config

--- app/js/webpack.prod.js
+++ app/js/webpack.prod.js
@@ -1,9 +1,15 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require("webpack");
 const baseConfig = require('./webpack.base.js');
 
 module.exports = {
   ...baseConfig,
   plugins: [
-    new UglifyJsPlugin()
+    new UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": '"production"'
+      }
+    }),
   ],
 };
