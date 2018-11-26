87b02d8d3e69be658b295ab975c30fb837f354fb
Jens Nyman <nymanjens.nj@gmail.com>
2018-11-26 10:48:17 +0100

    Fix JvmClockTest after merging with piga

--- app/jvm/src/test/scala/common/time/JvmClockTest.scala
+++ app/jvm/src/test/scala/common/time/JvmClockTest.scala
@@ -2,15 +2,14 @@ package common.time
 
 import java.time.{Duration, Instant}
 
-import com.google.inject.Guice
+import com.google.inject._
 import common.testing.{HookedSpecification, TestModule}
 import common.time.JavaTimeImplicits._
-import javax.inject.Inject
 import org.specs2.matcher.MatchResult
 
 class JvmClockTest extends HookedSpecification {
 
-  @Inject implicit private val jvmClock: JvmClock = null
+  @Inject private val jvmClock: JvmClock = null
 
   override def before() = {
     Guice.createInjector(new TestModule).injectMembers(this)
