44fbe86e787f65132eba40c9e033142658d7c8cb
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-24 23:15:43 +0100

    Add app-specific actions

--- app/js/shared/src/main/scala/flux/action/Actions.scala
+++ app/js/shared/src/main/scala/flux/action/Actions.scala
@@ -1,20 +1,25 @@
 package flux.action
 
-import flux.action.Actions.AddSongsToPlaylist.Placement
 import hydro.flux.action.Action
+import models.accounting.BalanceCheck
+import models.accounting.Transaction
+import models.accounting.TransactionGroup
 
 import scala.collection.immutable.Seq
 
 object Actions {
 
-  // **************** Media-related actions **************** //
-  case class AddSongsToPlaylist(songIds: Seq[Long], placement: Placement) extends Action
-  object AddSongsToPlaylist {
-    sealed trait Placement
-    object Placement {
-      object AfterCurrentSong extends Placement
-      object AtEnd extends Placement
-    }
-  }
-  case class RemoveEntriesFromPlaylist(playlistEntryIds: Seq[Long]) extends Action
+  // **************** Transaction[Group]-related actions **************** //
+  case class AddTransactionGroup(transactionsWithoutIdProvider: TransactionGroup => Seq[Transaction])
+      extends Action
+  case class UpdateTransactionGroup(transactionGroupWithId: TransactionGroup,
+                                    transactionsWithoutId: Seq[Transaction])
+      extends Action
+  case class RemoveTransactionGroup(transactionGroupWithId: TransactionGroup) extends Action
+
+  // **************** BalanceCheck-related actions **************** //
+  case class AddBalanceCheck(balanceCheckWithoutId: BalanceCheck) extends Action
+  case class UpdateBalanceCheck(existingBalanceCheck: BalanceCheck, newBalanceCheckWithoutId: BalanceCheck)
+      extends Action
+  case class RemoveBalanceCheck(existingBalanceCheck: BalanceCheck) extends Action
 }
