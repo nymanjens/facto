d1a00dc37e853777a1f0c519c9459b8b3c65da1c
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-26 20:16:15 +0100

    Synchronize jsfacades

--- app/js/shared/src/main/scala/jsfacades/Mousetrap.scala
+++ app/js/shared/src/main/scala/jsfacades/Mousetrap.scala
@@ -6,9 +6,22 @@ import app.scala2js.Converters._
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
-@JSImport("global-mousetrap", JSImport.Namespace)
-@js.native
-object Mousetrap extends js.Object {
+object Mousetrap {
 
-  def bindGlobal(key: String, callback: js.Function1[KeyboardEvent, Unit]): Unit = js.native
+  def bind(key: String, callback: js.Function1[KeyboardEvent, Unit]): Unit =
+    RegularMousetrap.bind(key, callback)
+  def bindGlobal(key: String, callback: js.Function1[KeyboardEvent, Unit]): Unit =
+    GlobalMousetrap.bindGlobal(key, callback)
+
+  @JSImport("mousetrap", JSImport.Namespace)
+  @js.native
+  object RegularMousetrap extends js.Object {
+    def bind(key: String, callback: js.Function1[KeyboardEvent, Unit]): Unit = js.native
+  }
+
+  @JSImport("global-mousetrap", JSImport.Namespace)
+  @js.native
+  object GlobalMousetrap extends js.Object {
+    def bindGlobal(key: String, callback: js.Function1[KeyboardEvent, Unit]): Unit = js.native
+  }
 }
---
+++ app/js/shared/src/main/scala/jsfacades/ReactContentEditable.scala
@@ -0,0 +1,46 @@
+package jsfacades
+
+import hydro.common.LoggingUtils
+import hydro.common.LoggingUtils.logExceptions
+import japgolly.scalajs.react.Children
+import japgolly.scalajs.react.JsComponent
+import org.scalajs.dom
+import org.scalajs.dom.raw.KeyboardEvent
+
+import scala.scalajs.js
+import app.scala2js.Converters._
+
+import scala.scalajs.js.annotation.JSImport
+
+object ReactContentEditable {
+
+  // **************** API ****************//
+  def apply(html: String, onChange: String => Unit, onKeyDown: KeyboardEvent => Unit = _ => {}) = {
+    val component = JsComponent[js.Object, Children.None, Null](RawComponent)
+    component(
+      Props(
+        html = html,
+        onChange = evt =>
+          logExceptions {
+            onChange(evt.target.value.asInstanceOf[String])
+        },
+        onKeyDown = onKeyDown).toJsObject)
+  }
+
+  // **************** Private inner types ****************//
+  @JSImport("react-contenteditable", JSImport.Namespace)
+  @js.native
+  private object RawComponent extends js.Object
+
+  private case class Props(html: String,
+                           onChange: js.Function1[js.Dynamic, Unit],
+                           onKeyDown: js.Function1[KeyboardEvent, Unit]) {
+    def toJsObject: js.Object =
+      js.Dynamic.literal(
+        html = html,
+        disabled = false,
+        onChange = onChange,
+        onKeyDown = onKeyDown
+      )
+  }
+}
