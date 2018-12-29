89dac7de46582baf754a1d01db15f303815ac945
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-29 16:24:24 +0100

    Revert "Combine AppJsEntityAccess + Impl"
    This reverts commit e57da124b3b61c0d1313418edc2bfad2787d67b5.

--- app/js/shared/src/main/scala/app/models/access/AppJsEntityAccess.scala
+++ app/js/shared/src/main/scala/app/models/access/AppJsEntityAccess.scala
@@ -2,14 +2,4 @@ package app.models.access
 
 import app.models.user.User
 
-import scala.collection.immutable.Seq
-
-final class AppJsEntityAccess(allUsers: Seq[User])(
-    implicit remoteDatabaseProxy: RemoteDatabaseProxy,
-    entityModificationPushClientFactory: EntityModificationPushClientFactory)
-    extends JsEntityAccessImpl
-    with AppEntityAccess {
-
-  override def newQuerySyncForUser() =
-    DbResultSet.fromExecutor(DbQueryExecutor.fromEntities(allUsers))
-}
+trait AppJsEntityAccess extends AppEntityAccess with JsEntityAccess
