e6f2175d6ec6ef357b6a9ec91879d71095684140
Jens Nyman <nymanjens.nj@gmail.com>
2019-02-12 20:56:56 +0000

    Fix UserStoreTest

--- app/shared/src/main/scala/app/common/testing/TestObjects.scala
+++ app/shared/src/main/scala/app/common/testing/TestObjects.scala
@@ -194,7 +194,7 @@ object TestObjects {
     idOption = Option(918273),
     lastUpdateTime = testLastUpdateTime,
   )
-  val testUserB: User = User(
+  def testUserB: User = User(
     loginName = "testUserB",
     passwordHash =
       "be196838736ddfd0007dd8b2e8f46f22d440d4c5959925cb49135abc9cdb01e84961aa43dd0ddb6ee59975eb649280d9f44088840af37451828a6412b9b574fc",
