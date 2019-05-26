8ac341c9f8dffd7e870f61d51da2c1b41beca26d
Jens Nyman <nymanjens.nj@gmail.com>
2019-05-26 20:29:24 +0200

    Fix compile error

--- app/js/shared/src/main/scala/app/models/access/AppJsEntityAccessImpl.scala
+++ app/js/shared/src/main/scala/app/models/access/AppJsEntityAccessImpl.scala
@@ -3,7 +3,7 @@ package app.models.access
 import app.models.user.User
 import hydro.models.access.DbQueryExecutor
 import hydro.models.access.DbResultSet
-import hydro.models.access.EntityModificationPushClientFactory
+import hydro.models.access.HydroPushSocketClientFactory
 import hydro.models.access.JsEntityAccessImpl
 import hydro.models.access.RemoteDatabaseProxy
 
@@ -11,7 +11,7 @@ import scala.collection.immutable.Seq
 
 private[access] final class AppJsEntityAccessImpl(allUsers: Seq[User])(
     implicit remoteDatabaseProxy: RemoteDatabaseProxy,
-    entityModificationPushClientFactory: EntityModificationPushClientFactory)
+    hydroPushSocketClientFactory: HydroPushSocketClientFactory)
     extends JsEntityAccessImpl
     with AppJsEntityAccess {
 
