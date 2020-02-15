08e2a2434f1160f8ae9bd9464447a12cd9307a5a
Jens Nyman <nymanjens.nj@gmail.com>
2020-02-15 21:39:10 +0100

    Fix compile errors

--- app/shared/src/main/scala/app/models/access/ModelFields.scala
+++ app/shared/src/main/scala/app/models/access/ModelFields.scala
@@ -28,108 +28,84 @@ object ModelFields {
   object User {
     private type E = User
 
-    case object id extends IdModelField[E]
-    case object loginName extends ModelField[String, E]("loginName", _.loginName, v => _.copy(loginName = v))
-    case object passwordHash
-        extends ModelField[String, E]("passwordHash", _.passwordHash, v => _.copy(passwordHash = v))
-    case object name extends ModelField[String, E]("name", _.name, v => _.copy(name = v))
-    case object isAdmin extends ModelField[Boolean, E]("isAdmin", _.isAdmin, v => _.copy(isAdmin = v))
-    case object expandCashFlowTablesByDefault
-        extends ModelField[Boolean, E](
-          "expandCashFlowTablesByDefault",
-          _.expandCashFlowTablesByDefault,
-          v => _.copy(expandCashFlowTablesByDefault = v))
-    case object expandLiquidationTablesByDefault
-        extends ModelField[Boolean, E](
-          "expandLiquidationTablesByDefault",
-          _.expandLiquidationTablesByDefault,
-          v => _.copy(expandLiquidationTablesByDefault = v))
+    val id = ModelField.forId[E]()
+    val loginName: ModelField[String, E] = ModelField("loginName", _.loginName, v => _.copy(loginName = v))
+    val passwordHash: ModelField[String, E] =
+      ModelField("passwordHash", _.passwordHash, v => _.copy(passwordHash = v))
+    val name: ModelField[String, E] = ModelField("name", _.name, v => _.copy(name = v))
+    val isAdmin: ModelField[Boolean, E] = ModelField("isAdmin", _.isAdmin, v => _.copy(isAdmin = v))
+    val expandCashFlowTablesByDefault: ModelField[Boolean, E] = ModelField(
+      "expandCashFlowTablesByDefault",
+      _.expandCashFlowTablesByDefault,
+      v => _.copy(expandCashFlowTablesByDefault = v))
+    val expandLiquidationTablesByDefault: ModelField[Boolean, E] = ModelField(
+      "expandLiquidationTablesByDefault",
+      _.expandLiquidationTablesByDefault,
+      v => _.copy(expandLiquidationTablesByDefault = v))
   }
 
   object Transaction {
     private type E = app.models.accounting.Transaction
 
-    case object id extends IdModelField[E]
-    case object transactionGroupId
-        extends ModelField[Long, E](
-          "transactionGroupId",
-          _.transactionGroupId,
-          v => _.copy(transactionGroupId = v))
-    case object issuerId extends ModelField[Long, E]("issuerId", _.issuerId, v => _.copy(issuerId = v))
-    case object beneficiaryAccountCode
-        extends ModelField[String, E](
-          "beneficiaryAccountCode",
-          _.beneficiaryAccountCode,
-          v => _.copy(beneficiaryAccountCode = v))
-    case object moneyReservoirCode
-        extends ModelField[String, E](
-          "moneyReservoirCode",
-          _.moneyReservoirCode,
-          v => _.copy(moneyReservoirCode = v))
-    case object categoryCode
-        extends ModelField[String, E]("categoryCode", _.categoryCode, v => _.copy(categoryCode = v))
-    case object description
-        extends ModelField[String, E]("description", _.description, v => _.copy(description = v))
-    case object flowInCents
-        extends ModelField[Long, E]("flowInCents", _.flowInCents, v => _.copy(flowInCents = v))
-    case object detailDescription
-        extends ModelField[String, E](
-          "detailDescription",
-          _.detailDescription,
-          v => _.copy(detailDescription = v))
-    case object tags extends ModelField[Seq[String], E]("tags", _.tags, v => _.copy(tags = v))
-    case object createdDate
-        extends ModelField[LocalDateTime, E]("createdDate", _.createdDate, v => _.copy(createdDate = v))
-    case object transactionDate
-        extends ModelField[LocalDateTime, E](
-          "transactionDate",
-          _.transactionDate,
-          v => _.copy(transactionDate = v))
-    case object consumedDate
-        extends ModelField[LocalDateTime, E]("consumedDate", _.consumedDate, v => _.copy(consumedDate = v))
+    val id = ModelField.forId[E]()
+    val transactionGroupId: ModelField[Long, E] =
+      ModelField("transactionGroupId", _.transactionGroupId, v => _.copy(transactionGroupId = v))
+    val issuerId: ModelField[Long, E] = ModelField("issuerId", _.issuerId, v => _.copy(issuerId = v))
+    val beneficiaryAccountCode: ModelField[String, E] =
+      ModelField("beneficiaryAccountCode", _.beneficiaryAccountCode, v => _.copy(beneficiaryAccountCode = v))
+    val moneyReservoirCode: ModelField[String, E] =
+      ModelField("moneyReservoirCode", _.moneyReservoirCode, v => _.copy(moneyReservoirCode = v))
+    val categoryCode: ModelField[String, E] =
+      ModelField("categoryCode", _.categoryCode, v => _.copy(categoryCode = v))
+    val description: ModelField[String, E] =
+      ModelField("description", _.description, v => _.copy(description = v))
+    val flowInCents: ModelField[Long, E] =
+      ModelField("flowInCents", _.flowInCents, v => _.copy(flowInCents = v))
+    val detailDescription: ModelField[String, E] =
+      ModelField("detailDescription", _.detailDescription, v => _.copy(detailDescription = v))
+    val tags: ModelField[Seq[String], E] = ModelField("tags", _.tags, v => _.copy(tags = v))
+    val createdDate: ModelField[LocalDateTime, E] =
+      ModelField("createdDate", _.createdDate, v => _.copy(createdDate = v))
+    val transactionDate: ModelField[LocalDateTime, E] =
+      ModelField("transactionDate", _.transactionDate, v => _.copy(transactionDate = v))
+    val consumedDate: ModelField[LocalDateTime, E] =
+      ModelField("consumedDate", _.consumedDate, v => _.copy(consumedDate = v))
   }
 
   object TransactionGroup {
     private type E = app.models.accounting.TransactionGroup
 
-    case object id extends IdModelField[E]
-    case object createdDate
-        extends ModelField[LocalDateTime, E]("createdDate", _.createdDate, v => _.copy(createdDate = v))
+    val id = ModelField.forId[E]()
+    val createdDate: ModelField[LocalDateTime, E] =
+      ModelField("createdDate", _.createdDate, v => _.copy(createdDate = v))
   }
 
   object BalanceCheck {
     private type E = app.models.accounting.BalanceCheck
 
-    case object id extends IdModelField[E]
-    case object issuerId extends ModelField[Long, E]("issuerId", _.issuerId, v => _.copy(issuerId = v))
-    case object moneyReservoirCode
-        extends ModelField[String, E](
-          "moneyReservoirCode",
-          _.moneyReservoirCode,
-          v => _.copy(moneyReservoirCode = v))
-    case object balanceInCents
-        extends ModelField[Long, E]("balanceInCents", _.balanceInCents, v => _.copy(balanceInCents = v))
-    case object createdDate
-        extends ModelField[LocalDateTime, E]("createdDate", _.createdDate, v => _.copy(createdDate = v))
-    case object checkDate
-        extends ModelField[LocalDateTime, E]("checkDate", _.checkDate, v => _.copy(checkDate = v))
+    val id = ModelField.forId[E]()
+    val issuerId: ModelField[Long, E] = ModelField("issuerId", _.issuerId, v => _.copy(issuerId = v))
+    val moneyReservoirCode: ModelField[String, E] =
+      ModelField("moneyReservoirCode", _.moneyReservoirCode, v => _.copy(moneyReservoirCode = v))
+    val balanceInCents: ModelField[Long, E] =
+      ModelField("balanceInCents", _.balanceInCents, v => _.copy(balanceInCents = v))
+    val createdDate: ModelField[LocalDateTime, E] =
+      ModelField("createdDate", _.createdDate, v => _.copy(createdDate = v))
+    val checkDate: ModelField[LocalDateTime, E] =
+      ModelField("checkDate", _.checkDate, v => _.copy(checkDate = v))
   }
 
   object ExchangeRateMeasurement {
     private type E = ExchangeRateMeasurement
 
-    case object id extends IdModelField[E]
-    case object date extends ModelField[LocalDateTime, E]("date", _.date, v => _.copy(date = v))
-    case object foreignCurrencyCode
-        extends ModelField[String, E](
-          "foreignCurrencyCode",
-          _.foreignCurrencyCode,
-          v => _.copy(foreignCurrencyCode = v))
-    case object ratioReferenceToForeignCurrency
-        extends ModelField[Double, E](
-          "ratioReferenceToForeignCurrency",
-          _.ratioReferenceToForeignCurrency,
-          v => _.copy(ratioReferenceToForeignCurrency = v))
+    val id = ModelField.forId[E]()
+    val date: ModelField[LocalDateTime, E] = ModelField("date", _.date, v => _.copy(date = v))
+    val foreignCurrencyCode: ModelField[String, E] =
+      ModelField("foreignCurrencyCode", _.foreignCurrencyCode, v => _.copy(foreignCurrencyCode = v))
+    val ratioReferenceToForeignCurrency: ModelField[Double, E] = ModelField(
+      "ratioReferenceToForeignCurrency",
+      _.ratioReferenceToForeignCurrency,
+      v => _.copy(ratioReferenceToForeignCurrency = v))
   }
 
   // **************** Field-related methods **************** //
