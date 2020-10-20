e05dcc33a7d5447163765bbf39e2928cf9916c21
Jens Nyman <nymanjens.nj@gmail.com>
2020-10-20 11:19:31 +0200

    [Bump version to v3.28.x02]

--- app/shared/src/main/scala/app/AppVersion.scala
+++ app/shared/src/main/scala/app/AppVersion.scala
@@ -2,5 +2,5 @@ package app
 
 /** Version string that gets incremented on every deploy. */
 object AppVersion {
-  val versionString: String = "3.28.x01"
+  val versionString: String = "3.28.x02"
 }
