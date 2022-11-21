c260a51f7ff43b6e96f992e20e3c7fd394883836
Jens Nyman <nymanjens.nj@gmail.com>
2022-11-21 21:43:10 +0100

    Add supportInflationCorrections to config.constants

--- app/shared/src/main/scala/app/models/accounting/config/Constants.scala
+++ app/shared/src/main/scala/app/models/accounting/config/Constants.scala
@@ -8,6 +8,9 @@ case class Constants(
     endowmentCategory: Category,
     liquidationDescription: String,
     zoneId: String,
+    // If true, then a button will be shown to enable/disable inflation corrections. These are made using the price
+    // index stored in the exchange rate of a special "<index>" currency.
+    supportInflationCorrections: Boolean,
 ) {
   requireNonNull(commonAccount, accountingCategory, endowmentCategory, liquidationDescription, zoneId)
 }
