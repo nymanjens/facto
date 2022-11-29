c615ed154272c5ac87fbd55babdaa16149e9ae7c
Jens Nyman <nymanjens.nj@gmail.com>
2022-11-29 12:04:23 +0000

    Move DateToBalanceFunction out of SummaryExchangeRateGainsStoreFactory

--- app/js/shared/src/main/scala/app/flux/stores/entries/factories/SummaryExchangeRateGainsStoreFactory.scala
+++ app/js/shared/src/main/scala/app/flux/stores/entries/factories/SummaryExchangeRateGainsStoreFactory.scala
@@ -2,15 +2,14 @@ package app.flux.stores.entries.factories
 
 import java.time.Duration
 import java.time.LocalTime
-
 import app.common.accounting.ComplexQueryFilter
+import app.common.accounting.DateToBalanceFunction
 import app.common.money.Currency
 import app.common.money.ExchangeRateManager
 import app.common.money.MoneyWithGeneralCurrency
 import app.common.money.ReferenceMoney
 import app.common.time.DatedMonth
 import app.flux.stores.entries.EntriesStore
-import app.flux.stores.entries.factories.SummaryExchangeRateGainsStoreFactory.DateToBalanceFunction
 import app.flux.stores.entries.factories.SummaryExchangeRateGainsStoreFactory.GainsForMonth
 import app.flux.stores.entries.factories.SummaryExchangeRateGainsStoreFactory.ExchangeRateGains
 import app.models.access.AppDbQuerySorting
@@ -272,69 +271,5 @@ object SummaryExchangeRateGainsStoreFactory {
       GainsForMonth(
         reservoirToGains = combineMapValues(gains.map(_.reservoirToGains))(_.sum).filterNot(_._2.isZero)
       )
-  }
-
-  private[SummaryExchangeRateGainsStoreFactory] final class DateToBalanceFunction(
-      dateToBalanceUpdates: SortedMap[LocalDateTime, DateToBalanceFunction.Update]
-  ) {
-    def apply(date: LocalDateTime): MoneyWithGeneralCurrency = {
-      dateToBalanceUpdates.to(date).values.last.balance
-    }
-
-    def updatesInRange(month: DatedMonth): SortedMap[LocalDateTime, DateToBalanceFunction.Update] =
-      dateToBalanceUpdates.range(month.startTime, month.startTimeOfNextMonth)
-  }
-
-  private[SummaryExchangeRateGainsStoreFactory] object DateToBalanceFunction {
-    case class Update(balance: MoneyWithGeneralCurrency, changeComparedToLast: MoneyWithGeneralCurrency)
-
-    final class Builder(initialDate: LocalDateTime, initialBalance: MoneyWithGeneralCurrency) {
-      private val dateToBalanceUpdates: mutable.SortedMap[LocalDateTime, Update] =
-        mutable.SortedMap(
-          initialDate -> Update(balance = initialBalance, changeComparedToLast = initialBalance)
-        )
-
-      def incrementLatestBalance(date: LocalDateTime, addition: MoneyWithGeneralCurrency): Unit = {
-        val (lastDate, lastBalance) = dateToBalanceUpdates.last
-        dateToBalanceUpdates.put(
-          maybeAddSomeSecondsToAvoidEdgeCases(date),
-          Update(balance = lastBalance.balance + addition, changeComparedToLast = addition),
-        )
-      }
-
-      def addBalanceUpdate(date: LocalDateTime, balance: MoneyWithGeneralCurrency): Unit = {
-        val (lastDate, lastBalance) = dateToBalanceUpdates.last
-        dateToBalanceUpdates.put(
-          maybeAddSomeSecondsToAvoidEdgeCases(date),
-          Update(balance = balance, changeComparedToLast = balance - lastBalance.balance),
-        )
-      }
-
-      def result: DateToBalanceFunction = {
-        new DateToBalanceFunction(SortedMap.apply(dateToBalanceUpdates.toSeq: _*))
-      }
-
-      /** Avoids duplicates dates and 00:00. Assumes that dates are provided to this builder in chronological order. */
-      private def maybeAddSomeSecondsToAvoidEdgeCases(date: LocalDateTime): LocalDateTime = {
-        var candidate = date
-
-        // Avoid LocalDateTimes that start at '00:00', which on the first day of the month interferes
-        // with DatedMonth.startOfNextMonth.
-        candidate = if (candidate.toLocalTime == LocalTime.MIN) {
-          candidate.plus(Duration.ofSeconds(1))
-        } else {
-          candidate
-        }
-
-        // Avoid dates that are already in the dateToBalanceUpdates map (assuming dates were presented in
-        // chronological order)
-        val (lastDate, lastBalance) = dateToBalanceUpdates.last
-        candidate =
-          if (lastDate < candidate) candidate
-          else lastDate.plus(Duration.ofSeconds(1))
-
-        candidate
-      }
-    }
   }
 }
